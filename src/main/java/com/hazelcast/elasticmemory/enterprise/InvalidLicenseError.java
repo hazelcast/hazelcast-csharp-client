f566ba8f98e97840a6e757e6b6d66075f248411f
Mehmet Dogan <mehmet@hazelcast.com>
2011-09-29 08:48:33 +0000

    cleanup
    git-svn-id: https://svn.hazelcast.com/svn/elastic-memory/trunk@20 51353bd8-8dbf-4efe-9e6d-88ec6fb4063a

--- src/main/java/com/hazelcast/elasticmemory/enterprise/InvalidLicenseError.java
+++
@@ -1,9 +0,0 @@
-package com.hazelcast.elasticmemory.enterprise;
-
-public class InvalidLicenseError extends Error {
-
-	public InvalidLicenseError() {
-		super("Invalid license file! Please contact sales@hazelcast.com");
-	}
-
-}
