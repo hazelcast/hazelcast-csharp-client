0e530a2cdf32077585baf7430f7161d90306f2ab
Mehmet Dogan <mehmet@hazelcast.com>
2011-10-12 06:28:24 +0000

    git-svn-id: https://svn.hazelcast.com/svn/elastic-memory/trunk@45 51353bd8-8dbf-4efe-9e6d-88ec6fb4063a

--- src/main/java/com/hazelcast/enterprise/KeyLoadStoreUtil.java
+++ src/main/java/com/hazelcast/enterprise/KeyLoadStoreUtil.java
@@ -10,7 +10,6 @@ import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
 
 public class KeyLoadStoreUtil {
 	
