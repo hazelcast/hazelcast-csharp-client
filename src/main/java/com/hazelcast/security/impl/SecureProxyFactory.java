7fa9a82e77e1a95540c13d94ac386df0ad702c19
Mehmet Dogan <mehmet@hazelcast.com>
2011-11-01 14:42:45 +0000

    Added IdGenerator security impl
    git-svn-id: https://svn.hazelcast.com/svn/elastic-memory/trunk@67 51353bd8-8dbf-4efe-9e6d-88ec6fb4063a

--- src/main/java/com/hazelcast/security/impl/SecureProxyFactory.java
+++ src/main/java/com/hazelcast/security/impl/SecureProxyFactory.java
@@ -23,9 +23,7 @@ public class SecureProxyFactory extends DefaultProxyFactory implements ProxyFact
 
 	@Override
 	public MProxy createMapProxy(String name) {
-		if(name.startsWith(Prefix.MAP_FOR_QUEUE)
-				|| name.startsWith(Prefix.MAP_HAZELCAST)
-				|| name.startsWith(Prefix.MAP_OF_LIST)) {
+		if(isInternalProxy(name)) {
 			return super.createMapProxy(name); 
 		}
 		return new SecureMProxy(node, super.createMapProxy(name));
@@ -63,6 +61,9 @@ public class SecureProxyFactory extends DefaultProxyFactory implements ProxyFact
 
 	@Override
 	public AtomicNumberProxy createAtomicNumberProxy(String name) {
+		if(isInternalProxy(name)) {
+			return super.createAtomicNumberProxy(name);
+		}
 		return new SecureAtomicNumberProxy(node, super.createAtomicNumberProxy(name));
 	}
 
@@ -78,7 +79,7 @@ public class SecureProxyFactory extends DefaultProxyFactory implements ProxyFact
 
 	@Override
 	public IdGeneratorProxy createIdGeneratorProxy(String name) {
-		return super.createIdGeneratorProxy(name);
+		return new SecureIdGeneratorProxy(node, super.createIdGeneratorProxy(name));
 	}
 
 	@Override
@@ -97,32 +98,45 @@ public class SecureProxyFactory extends DefaultProxyFactory implements ProxyFact
 
 	public void checkProxy(ProxyKey proxyKey) throws SecurityException {
 		final String name = proxyKey.getName();
-		final String action = SecurityConstants.ACTION_CREATE;
-		final String actualName = getShortInstanceName(name);
-		Permission p = null;
-		
-		if (name.startsWith(Prefix.MAP)) {
-			p = new MapPermission(actualName, action);
-		} else if (name.startsWith(Prefix.QUEUE)) {
-			p = new QueuePermission(actualName, action);
-		} else if (name.startsWith(Prefix.TOPIC)) {
-			p = new TopicPermission(actualName, action);
-		} else if (name.startsWith(Prefix.AS_LIST)) {
-			p = new ListPermission(actualName, action);
-		} else if (name.startsWith(Prefix.MULTIMAP)) {
-			p = new MultiMapPermission(actualName, action);
-		} else if (name.startsWith(Prefix.SET)) {
-			p = new SetPermission(actualName, action);
-		} else if (name.startsWith(Prefix.ATOMIC_NUMBER)) {
-			p = new AtomicNumberPermission(actualName, action);
-		} else if (name.startsWith(Prefix.IDGEN)) {
-		} else if (name.startsWith(Prefix.SEMAPHORE)) {
-			p = new SemaphorePermission(actualName, action);
-		} else if (name.startsWith(Prefix.COUNT_DOWN_LATCH)) {
-			p = new CountDownLatchPermission(actualName, action);
-		} else if (name.equals("lock")) {
-			p = new LockPermission(proxyKey.getKey(), action);
+		if(!isInternalProxy(name)) {
+			final String action = SecurityConstants.ACTION_CREATE;
+			final String shortName = getShortInstanceName(name);
+			Permission p = null;
+			
+			if (name.startsWith(Prefix.MAP)) {
+				p = new MapPermission(shortName, action);
+			} else if (name.startsWith(Prefix.QUEUE)) {
+				p = new QueuePermission(shortName, action);
+			} else if (name.startsWith(Prefix.TOPIC)) {
+				p = new TopicPermission(shortName, action);
+			} else if (name.startsWith(Prefix.AS_LIST)) {
+				p = new ListPermission(shortName, action);
+			} else if (name.startsWith(Prefix.MULTIMAP)) {
+				p = new MultiMapPermission(shortName, action);
+			} else if (name.startsWith(Prefix.SET)) {
+				p = new SetPermission(shortName, action);
+			} else if (name.startsWith(Prefix.ATOMIC_NUMBER)) {
+				p = new AtomicNumberPermission(shortName, action);
+			} else if (name.startsWith(Prefix.IDGEN)) {
+				p = new IdGeneratorPermission(shortName, action);
+			} else if (name.startsWith(Prefix.SEMAPHORE)) {
+				p = new SemaphorePermission(shortName, action);
+			} else if (name.startsWith(Prefix.COUNT_DOWN_LATCH)) {
+				p = new CountDownLatchPermission(shortName, action);
+			} else if (name.equals("lock")) {
+				p = new LockPermission(proxyKey.getKey(), action);
+			}
+			SecurityUtil.checkPermission(node.securityContext, p);
 		}
-		SecurityUtil.checkPermission(node.securityContext, p);
+	}
+	
+	private boolean isInternalProxy(String name) {
+		return (name.startsWith(Prefix.MAP_FOR_QUEUE)
+				|| name.startsWith(Prefix.MAP_HAZELCAST)
+				|| name.startsWith(Prefix.MAP_OF_LIST)
+				|| name.startsWith(Prefix.ATOMIC_NUMBER_HAZELCAST)
+				|| name.startsWith(Prefix.QUEUE_HAZELCAST)
+				|| name.startsWith(Prefix.TOPIC_HAZELCAST)
+		); 
 	}
 }
