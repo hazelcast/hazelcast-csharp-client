44a36bf9024ce3869833bfbc7047e57936fdece1
Mehmet Dogan <mehmet@hazelcast.com>
2011-11-25 07:21:21 +0000

    git-svn-id: https://svn.hazelcast.com/svn/elastic-memory/trunk@74 51353bd8-8dbf-4efe-9e6d-88ec6fb4063a

--- src/main/java/com/hazelcast/elasticmemory/storage/OffHeapStorageSupport.java
+++ src/main/java/com/hazelcast/elasticmemory/storage/OffHeapStorageSupport.java
@@ -2,6 +2,8 @@ package com.hazelcast.elasticmemory.storage;
 
 import static com.hazelcast.elasticmemory.util.MathUtil.*;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.util.logging.Level;
 
 import com.hazelcast.logging.ILogger;
@@ -37,17 +39,17 @@ public abstract class OffHeapStorageSupport {
 		this.segmentSizeInMb = segmentSizeInMb;
 	}
 	
-	protected final void destroy(final Destroyable... destroyables) {
-		for (int i = 0; i < destroyables.length; i++) {
-			if(destroyables[i] != null) {
-				destroyables[i].destroy();
-				destroyables[i] = null;
+	protected final void destroy(final Closeable... resources) {
+		for (int i = 0; i < resources.length; i++) {
+			if(resources[i] != null) {
+				try {
+					resources[i].close();
+				} catch (IOException e) {
+					logger.log(Level.WARNING, e.getMessage(), e);
+				}
+				resources[i] = null;
 			}
 		}
 		System.gc();
-	}
-
-	protected interface Destroyable {
-		void destroy();
 	}
 }
