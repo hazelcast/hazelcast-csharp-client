1178dac3d337129a15eabe2e355d9bb8c7637094
Fuad Malikov <fuad@hazelcast.com>
2012-01-17 16:56:56 +0000

    git-svn-id: https://svn.hazelcast.com/svn/elastic-memory/trunk@85 51353bd8-8dbf-4efe-9e6d-88ec6fb4063a

---
+++ dotnet/hazelcast-net/src/Hazelcast/Hazelcast.Client/Hazelcast.Client/Core/IList.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Hazelcast.Client
+{
+	public interface IList<E>:ICollection<E>, System.Collections.Generic.IList<E>
+	{
+		
+		
+	}
+}
+
