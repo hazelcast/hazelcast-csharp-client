e4f12bae9af809fdbc77861e6fa778ac991f54d9
Fuad Malikov <fuad@hazelcast.com>
2012-01-17 16:56:36 +0000

    git-svn-id: https://svn.hazelcast.com/svn/elastic-memory/trunk@84 51353bd8-8dbf-4efe-9e6d-88ec6fb4063a

--- dotnet/hazelcast-net/src/Hazelcast/Hazelcast.Client/Hazelcast.Client/Client/HazelcastClient.cs
+++ dotnet/hazelcast-net/src/Hazelcast/Hazelcast.Client/Hazelcast.Client/Client/HazelcastClient.cs
@@ -6,11 +6,12 @@ using System.Collections.Generic;
 using Hazelcast.Client.Impl;
 namespace Hazelcast.Client
 {
-	public class HazelcastClient
+	public class HazelcastClient: HazelcastInstance
 	{
 		private readonly OutThread outThread;		
 		private readonly InThread inThread;
 		private readonly ListenerManager listenerManager;
+		private readonly String groupName;
 		ConcurrentDictionary<long, Call> calls = new ConcurrentDictionary<long, Call>();
 		Dictionary<Object, Object> proxies = new Dictionary<Object, Object>();
 		
@@ -21,7 +22,7 @@ namespace Hazelcast.Client
 			this.listenerManager = ListenerManager.start();
 			this.outThread = OutThread.start(tcp, calls);
 			this.inThread = InThread.start(tcp, calls, listenerManager);
-			
+			this.groupName = groupName;
 			
 		}
 		
@@ -29,16 +30,24 @@ namespace Hazelcast.Client
 			return new HazelcastClient(groupName, groupPass, address);
 		}
 		
+		public String getName(){
+			return groupName;	
+		}
+		
 		public IMap<K,V> getMap<K,V>(String name){
-			return (IMap<K,V>)getClientProxy(Prefix.MAP + name, ()=> new MapClientProxy<K, V>(outThread, Prefix.MAP + name, listenerManager));
+			return (IMap<K,V>)getClientProxy(Prefix.MAP + name, ()=> new MapClientProxy<K, V>(outThread, Prefix.MAP + name, listenerManager, this));
 		}
 		
 		public IQueue<E> getQueue<E>(String name){
-			return (IQueue<E>)getClientProxy(Prefix.QUEUE + name, () => new QueueClientProxy<E>(outThread, Prefix.QUEUE +name, listenerManager));
+			return (IQueue<E>)getClientProxy(Prefix.QUEUE + name, () => new QueueClientProxy<E>(outThread, Prefix.QUEUE +name, listenerManager, this));
 		}
 		
 		public ITopic<E> getTopic<E>(String name){
-			return (ITopic<E>)getClientProxy(Prefix.TOPIC + name, () => new TopicClientProxy<E>(outThread, Prefix.TOPIC +name, listenerManager));
+			return (ITopic<E>)getClientProxy(Prefix.TOPIC + name, () => new TopicClientProxy<E>(outThread, Prefix.TOPIC +name, listenerManager, this));
+		}
+		
+		public ISet<E> getSet<E>(String name){
+			return (ISet<E>)getClientProxy(Prefix.SET + name, () => new SetClientProxy<E>(outThread, Prefix.TOPIC +name, listenerManager, this));
 		}
 		
 		private Object getClientProxy(Object o, Func<Object> func)
@@ -53,6 +62,12 @@ namespace Hazelcast.Client
 	        	}
         	}
         	return proxies[o];
+		}
+		
+		public void destroy(String name){
+			lock(proxies){
+				proxies.Remove(name);
+			}
 		}
 	}
 }
---
+++ dotnet/hazelcast-net/src/Hazelcast/Hazelcast.Client/Hazelcast.Client/Client/SetClientProxy.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using Hazelcast.Client.Impl;
+using Hazelcast.Core;
+using Hazelcast.Client.IO;
+using Hazelcast.IO;
+using Hazelcast.Impl;
+
+
+namespace Hazelcast.Client
+{
+	public class SetClientProxy<E> : CollectionClientProxy<E>, ISet<E>
+	{
+		
+		public SetClientProxy (OutThread outThread, String name, ListenerManager listenerManager, HazelcastClient client):base(outThread, name, listenerManager, client)
+		{
+			
+		}
+		
+			
+		public override void addItemListener(ItemListener<E> listener, bool includeValue){
+			
+		}
+			
+		public override void removeItemListener(ItemListener<E> listener){
+			
+		}
+		
+		public override bool Contains(E e){
+        	return proxyHelper.doOp<bool>(ClusterOperation.CONCURRENT_MAP_CONTAINS_KEY, e, null);
+    	}
+		
+	
+    	public override int size() {
+        	return proxyHelper.doOp<int>(ClusterOperation.CONCURRENT_MAP_SIZE, null, null);
+    	}
+		
+		
+		public override void Add(E e){
+			proxyHelper.doOp<object>(ClusterOperation.CONCURRENT_MAP_ADD_TO_SET, e, null);
+		}
+		
+		public override System.Collections.Generic.IList<E> entries(){
+			return proxyHelper.entries<E>(null);
+		}
+	}
+}
+
