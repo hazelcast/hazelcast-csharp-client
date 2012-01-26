a225de7244f7c2facc6835c41d295c89e65b1635
Fuad Malikov <fuad@hazelcast.com>
2012-01-26 09:11:49 +0000

    LifecycleService shutdown and changed the threads to extend from ClientThread
    git-svn-id: https://svn.hazelcast.com/svn/elastic-memory/trunk@103 51353bd8-8dbf-4efe-9e6d-88ec6fb4063a

---
+++ dotnet/hazelcast-net/src/Hazelcast/Hazelcast.Client/Hazelcast.Client/Client/ClientThread.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Threading;
+
+namespace Hazelcast.Client
+{
+	public abstract class ClientThread
+	{
+		
+		protected volatile bool running = true;
+    	protected volatile bool terminated = false;
+		protected readonly Object monitor = new Object();
+		
+		public ClientThread ()
+		{
+		}
+		
+		protected abstract void customRun();
+		
+		public void run() {
+	        try {
+	            while (running) {
+	            	customRun();
+	            }
+	        } finally {
+	            terminate();
+	        }
+		}	
+		public void shutdown() {
+	        if(terminated){
+	            return;
+	        }
+
+	        lock (monitor) {
+	            running = false;
+	            while (!terminated) {
+	                Monitor.Wait(monitor);
+	            }
+	        }
+		}
+
+	    protected void terminate() {
+	        lock (monitor) {
+	            terminated = true;
+	            Monitor.PulseAll(monitor);
+	        }
+	    }
+	}
+}
+
