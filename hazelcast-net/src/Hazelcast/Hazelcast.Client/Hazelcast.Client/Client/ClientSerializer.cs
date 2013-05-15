3727db2e9ceb8a259d1706f270f05a1a9fecc522
Fuad Malikov <fuad@hazelcast.com>
2013-05-15 12:26:36 -0700

    fixed UTF reading and writing

--- hazelcast-net/src/Hazelcast/Hazelcast.Client/Hazelcast.Client/Client/ClientSerializer.cs
+++ hazelcast-net/src/Hazelcast/Hazelcast.Client/Hazelcast.Client/Client/ClientSerializer.cs
@@ -6,26 +6,28 @@ namespace Hazelcast.Client.IO
 	{
 		private ITypeSerializer cs = new CustomSerializerAdaptor(new DefaultSerializer());
 		private ITypeSerializer ds = new DataSerializer();
-		
-		
-		public byte[] toByte(Object obj){
+
+		public byte[] toByte(Object obj)
+		{
 			MemoryStream stream = new MemoryStream();
 			ITypeSerializer ts = ds.isSuitable(obj)? ds: cs;
 			stream.Seek(0, SeekOrigin.Begin);
-			BinaryWriter writer = new BinaryWriter(stream);
+			BinaryWriter writer = new BinaryWriter(stream, System.Text.Encoding.UTF8);
 			writer.Write((byte)ts.getTypeId());
 			ts.write(writer, obj);
 			return stream.ToArray();
 		}
 		
-		public Object toObject(byte[] bytes){
+		public Object toObject(byte[] bytes)
+		{
 			MemoryStream stream = new MemoryStream(bytes);
-			BinaryReader reader = new BinaryReader(stream);
+			BinaryReader reader = new BinaryReader(stream, System.Text.Encoding.UTF8);
 			byte typeID = reader.ReadByte();
 			ITypeSerializer serializer = (ds.getTypeId()== typeID)? ds: cs;
 			return serializer.read(reader);
 		}
 		
 	}
+
 }
 
