3e8a17ad818eaf9ef6f1e67f1db30e1c4c9a1cff
Asım Arslan <arslanasim@gmail.com>
2013-12-03 14:31:03 +0200

    project refactor from mono to ms

---
+++ Hazelcast.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Hazelcast")]
+[assembly: AssemblyDescription("Hazelcast .Net Client")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hazelcast Inc.")]
+[assembly: AssemblyProduct("Hazelcast.Net")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("7122f77d-6770-4d76-a711-990cad558b3f")]
+
+[assembly: InternalsVisibleTo("Hazelcast.Test")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("2.6.0.0")]
+[assembly: AssemblyFileVersion("2.6.0.0")]
