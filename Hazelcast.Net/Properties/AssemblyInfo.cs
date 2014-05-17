66da91c6272f846d6d21bd663d28f37ca0ae6bb8
Asım Arslan <arslanasim@gmail.com>
2014-05-17 21:36:16 +0300

    release 3.3.0

--- Hazelcast.Net/Properties/AssemblyInfo.cs
+++ Hazelcast.Net/Properties/AssemblyInfo.cs
@@ -13,12 +13,12 @@
 [assembly: System.Reflection.AssemblyCompany("Hazelcast Inc.")]
 [assembly: System.Reflection.AssemblyProduct("Hazelcast Enterprise Edition")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright (c) 2008-2014, Hazelcast, Inc")]
-[assembly: System.Reflection.AssemblyConfiguration("Commit bf7aa91")]
+[assembly: System.Reflection.AssemblyConfiguration("Commit 366bb5a")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyVersion("3.3.0.1")]
-[assembly: System.Reflection.AssemblyFileVersion("3.3.0.1")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.3.0.1")]
+[assembly: System.Reflection.AssemblyVersion("3.3.0.3")]
+[assembly: System.Reflection.AssemblyFileVersion("3.3.0.3")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.3.0.3")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Hazelcast.Test")]
 
 
