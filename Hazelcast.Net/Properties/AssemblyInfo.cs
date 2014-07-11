499712d96259d727e6f17b932e16cbb3b8deacc5
Asım Arslan <arslanasim@gmail.com>
2014-07-11 14:56:18 +0300

    UTF serialization fix, release 3.3-RC2

--- Hazelcast.Net/Properties/AssemblyInfo.cs
+++ Hazelcast.Net/Properties/AssemblyInfo.cs
@@ -13,12 +13,12 @@
 [assembly: System.Reflection.AssemblyCompany("Hazelcast Inc.")]
 [assembly: System.Reflection.AssemblyProduct("Hazelcast Enterprise Edition")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright (c) 2008-2014, Hazelcast, Inc")]
-[assembly: System.Reflection.AssemblyConfiguration("Commit 2a11ab0")]
+[assembly: System.Reflection.AssemblyConfiguration("Commit 32e5966")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyVersion("3.3.0.7")]
-[assembly: System.Reflection.AssemblyFileVersion("3.3.0.7")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.3.0.7")]
+[assembly: System.Reflection.AssemblyVersion("3.3.0.8")]
+[assembly: System.Reflection.AssemblyFileVersion("3.3.0.8")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.3.0.8")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Hazelcast.Test")]
 
 
