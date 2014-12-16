786e5e006f684ef9229462716efc14e6b113e84c
Bilal Yasar <bilalyasar45@gmail.com>
2014-12-16 14:34:52 +0200

    Update csharp.bat

--- script/csharp.bat
+++ script/csharp.bat
@@ -1,4 +1,4 @@
-call mvn dependency:get -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=com.hazelcast:hazelcast:3.4-SNAPSHOT -Ddest=hazelcast.jar
+call mvn dependency:get -DrepoUrl=https://oss.sonatype.org/content/repositories/snapshots -Dartifact=com.hazelcast:hazelcast:3.5-SNAPSHOT -Ddest=hazelcast.jar
 start "hazelcast" java -cp hazelcast.jar -Dhazelcast.config=script\hazelcast.xml com.hazelcast.core.server.StartServer
 ping -n 10 127.0.0.1 >nul
 nunit-console /xml:"console-text.xml" "Hazelcast.Test/Hazelcast.Test.nunit"
