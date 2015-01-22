d75f6c353130b14a19ad8cef6153f4b40da198d7
Asım Arslan <arslanasim@gmail.com>
2015-01-22 10:36:48 +0200

    release is know strong named dll

--- Hazelcast.Net/Properties/AssemblyInfo.cs
+++ Hazelcast.Net/Properties/AssemblyInfo.cs
@@ -13,12 +13,12 @@
 [assembly: System.Reflection.AssemblyCompany("Hazelcast Inc.")]
 [assembly: System.Reflection.AssemblyProduct("Hazelcast Enterprise Edition")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright (c) 2008-2014, Hazelcast, Inc")]
-[assembly: System.Reflection.AssemblyConfiguration("Commit 0ff56d2")]
+[assembly: System.Reflection.AssemblyConfiguration("Commit 475324b")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyVersion("3.5.0.0")]
-[assembly: System.Reflection.AssemblyFileVersion("3.5.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.5.0.0")]
+[assembly: System.Reflection.AssemblyVersion("3.5.0.3")]
+[assembly: System.Reflection.AssemblyFileVersion("3.5.0.3")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.5.0.3")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Hazelcast.Test")]
 
 
