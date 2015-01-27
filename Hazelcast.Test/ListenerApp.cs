0ea26cd144d3dae254eecb3689ab1726edcf0894
Asım Arslan <arslanasim@gmail.com>
2015-01-27 20:21:10 +0200

    client tests

---
+++ Hazelcast.Test/ListenerApp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using Hazelcast.Client;
+using Hazelcast.Config;
+using Hazelcast.Core;
+
+namespace Hazelcast.Test
+{
+    public class ListenerApp
+    {
+
+        static void Main(string[] args)
+        {
+            Environment.SetEnvironmentVariable("hazelcast.logging.type", "console");
+
+            var clientConfig = new ClientConfig();
+            clientConfig.GetNetworkConfig().AddAddress("127.0.0.1");
+            var hc = HazelcastClient.NewHazelcastClient(clientConfig);
+            var listener1 = new EntryAdapter<string, string>(
+                @event => Console.WriteLine("ADD"),
+                @event => Console.WriteLine("REMOVE"),
+                @event => Console.WriteLine("UPDATE"),
+                @event => Console.WriteLine("EVICTED"));
+
+            var map = hc.GetMap<string, string>("default");
+            string reg1 = map.AddEntryListener(listener1, false);
+
+        }
+
+    }
+}
