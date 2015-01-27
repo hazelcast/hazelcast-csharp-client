d983edaf479797f2f0a04aa1b16e576168db2a73
Bilal Yasar <bilalyasar45@gmail.com>
2015-01-27 14:38:24 +0200

    Update AssemblyInfo.cs

--- Hazelcast.Net/Properties/AssemblyInfo.cs
+++ Hazelcast.Net/Properties/AssemblyInfo.cs
@@ -16,9 +16,9 @@
 [assembly: System.Reflection.AssemblyConfiguration("Commit cfcea21")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyVersion("3.4.0.3")]
-[assembly: System.Reflection.AssemblyFileVersion("3.4.0.3")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.4.0.3")]
+[assembly: System.Reflection.AssemblyVersion("3.4.1.0")]
+[assembly: System.Reflection.AssemblyFileVersion("3.4.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.4.1.0")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Hazelcast.Test")]
 
 
