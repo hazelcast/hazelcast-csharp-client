fecddcd1f45f71928cc2cd1f859b045319f222b9
Asım Arslan <arslanasim@gmail.com>
2015-06-24 14:20:20 +0300

    Update release.bat

--- script/release.bat
+++ script/release.bat
@@ -3,7 +3,7 @@ echo starting build.....
 MSBuild.exe ..\Hazelcast.Net\Hazelcast.Net.csproj /t:Version
 MSBuild.exe ..\Hazelcast.Net\Hazelcast.Net.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:Release
 
-REM MSBuild.exe ..\Hazelcast.Test\Hazelcast.Test.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:Build
+MSBuild.exe ..\Hazelcast.Test\Hazelcast.Test.csproj /p:Configuration=Debug /p:Platform=AnyCPU /t:Build
 REM START HAZELCAST SERVER
 REM MSBuild.exe ..\Hazelcast.Test\Hazelcast.Test.csproj /p:Configuration=Release /p:Platform=AnyCPU /t:RunTests
 REM STOP HAZELCAST SERVER
