91d373a774d6cd009059eb04f7a523810cc5d593
Can Gencer <cgencer@gmail.com>
2015-08-10 17:37:51 +0200

    added ClientDestroyProxyCodec

--- Hazelcast.Net/Hazelcast.Client.Protocol.Codec/ClientDestroyProxyCodec.cs
+++ Hazelcast.Net/Hazelcast.Client.Protocol.Codec/ClientDestroyProxyCodec.cs
@@ -1,84 +1,59 @@
 using Hazelcast.Client.Protocol;
 using Hazelcast.Client.Protocol.Util;
-using Hazelcast.Net.Ext;
+using Hazelcast.IO;
+using Hazelcast.IO.Serialization;
+using System.Collections.Generic;
 
 namespace Hazelcast.Client.Protocol.Codec
 {
-	internal sealed class ClientDestroyProxyCodec
-	{
-		public static readonly ClientMessageType RequestType = ClientMessageType.ClientDestroyproxy;
+    internal sealed class ClientDestroyProxyCodec
+    {
 
-		public const int ResponseType = 100;
+        public static readonly ClientMessageType RequestType = ClientMessageType.ClientDestroyProxy;
+        public const int ResponseType = 100;
+        public const bool Retryable = false;
 
-		public const bool Retryable = false;
+        //************************ REQUEST *************************//
 
-		public class RequestParameters
-		{
-			public static readonly ClientMessageType Type = RequestType;
+        public class RequestParameters
+        {
+            public static readonly ClientMessageType TYPE = RequestType;
+            public string name;
+            public string serviceName;
 
-			public string name;
+            public static int CalculateDataSize(string name, string serviceName)
+            {
+                int dataSize = ClientMessage.HeaderSize;
+                dataSize += ParameterUtil.CalculateDataSize(name);
+                dataSize += ParameterUtil.CalculateDataSize(serviceName);
+                return dataSize;
+            }
+        }
 
-			public string serviceName;
+        public static ClientMessage EncodeRequest(string name, string serviceName)
+        {
+            int requiredDataSize = RequestParameters.CalculateDataSize(name, serviceName);
+            ClientMessage clientMessage = ClientMessage.CreateForEncode(requiredDataSize);
+            clientMessage.SetMessageType((int)RequestType);
+            clientMessage.SetRetryable(Retryable);
+            clientMessage.Set(name);
+            clientMessage.Set(serviceName);
+            clientMessage.UpdateFrameLength();
+            return clientMessage;
+        }
 
-			//************************ REQUEST *************************//
-			public static int CalculateDataSize(string name, string serviceName)
-			{
-				int dataSize = ClientMessage.HeaderSize;
-				dataSize += ParameterUtil.CalculateStringDataSize(name);
-				dataSize += ParameterUtil.CalculateStringDataSize(serviceName);
-				return dataSize;
-			}
-		}
+        //************************ RESPONSE *************************//
 
-		public static ClientMessage EncodeRequest(string name, string serviceName)
-		{
-			int requiredDataSize = ClientDestroyProxyCodec.RequestParameters.CalculateDataSize(name, serviceName);
-			ClientMessage clientMessage = ClientMessage.CreateForEncode(requiredDataSize);
-			clientMessage.SetMessageType(RequestType.Id());
-			clientMessage.SetRetryable(Retryable);
-			clientMessage.Set(name);
-			clientMessage.Set(serviceName);
-			clientMessage.UpdateFrameLength();
-			return clientMessage;
-		}
 
-		public static ClientDestroyProxyCodec.RequestParameters DecodeRequest(ClientMessage clientMessage)
-		{
-			ClientDestroyProxyCodec.RequestParameters parameters = new ClientDestroyProxyCodec.RequestParameters();
-			string name;
-			name = null;
-			name = clientMessage.GetStringUtf8();
-			parameters.name = name;
-			string serviceName;
-			serviceName = null;
-			serviceName = clientMessage.GetStringUtf8();
-			parameters.serviceName = serviceName;
-			return parameters;
-		}
+        public class ResponseParameters
+        {
+        }
 
-		public class ResponseParameters
-		{
-			//************************ RESPONSE *************************//
-			public static int CalculateDataSize()
-			{
-				int dataSize = ClientMessage.HeaderSize;
-				return dataSize;
-			}
-		}
+        public static ResponseParameters DecodeResponse(IClientMessage clientMessage)
+        {
+            ResponseParameters parameters = new ResponseParameters();
+            return parameters;
+        }
 
-		public static ClientMessage EncodeResponse()
-		{
-			int requiredDataSize = ClientDestroyProxyCodec.ResponseParameters.CalculateDataSize();
-			ClientMessage clientMessage = ClientMessage.CreateForEncode(requiredDataSize);
-			clientMessage.SetMessageType(ResponseType);
-			clientMessage.UpdateFrameLength();
-			return clientMessage;
-		}
-
-		public static ClientDestroyProxyCodec.ResponseParameters DecodeResponse(ClientMessage clientMessage)
-		{
-			ClientDestroyProxyCodec.ResponseParameters parameters = new ClientDestroyProxyCodec.ResponseParameters();
-			return parameters;
-		}
-	}
+    }
 }
--- Hazelcast.Net/Hazelcast.Client.Protocol.Codec/ClientMessageType.cs
+++ Hazelcast.Net/Hazelcast.Client.Protocol.Codec/ClientMessageType.cs
@@ -1,46 +1,24 @@
-using Hazelcast.Net.Ext;
-
 namespace Hazelcast.Client.Protocol.Codec
 {
-	[System.Serializable]
-	internal sealed class ClientMessageType
-	{
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientAuthentication = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0x2)));
+    internal enum ClientMessageType
+    {
 
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientAuthenticationcustom = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0x3)));
+        ClientAuthentication = 0x2,
+        ClientAuthenticationCustom = 0x3,
+        ClientMembershipListener = 0x4,
+        ClientCreateProxy = 0x5,
+        ClientDestroyProxy = 0x6,
+        ClientGetPartitions = 0x8,
+        ClientRemoveAllListeners = 0x9,
+        ClientAddPartitionLostListener = 0xa,
+        ClientRemovePartitionLostListener = 0xb,
+        ClientGetDistributedObject = 0xc,
+        ClientAddDistributedObjectListener = 0xd,
+        ClientRemoveDistributedObjectListener = 0xe,
+        ClientPing = 0xf
 
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientMembershiplistener = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0x4)));
+    }
 
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientCreateproxy = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0x5)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientDestroyproxy = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0x6)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientGetpartitions = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0x8)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientRemovealllisteners = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0x9)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientAddpartitionlostlistener = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0xa)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientRemovepartitionlostlistener = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0xb)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientGetdistributedobject = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0xc)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientAdddistributedobjectlistener = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0xd)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientRemovedistributedobjectlistener = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0xe)));
-
-		public static readonly Hazelcast.Client.Protocol.Codec.ClientMessageType ClientPing = new Hazelcast.Client.Protocol.Codec.ClientMessageType(unchecked((int)(0xf)));
-
-		private readonly int id;
-
-		internal ClientMessageType(int messageType)
-		{
-			this.id = messageType;
-		}
-
-		public int Id()
-		{
-			return id;
-		}
-	}
 }
+
+
