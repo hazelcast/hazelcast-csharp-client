fc3715d9c08857548b991ebad7be4e1906fdda2d
Ubuntu <devops@hazelcast.com>
2015-11-17 10:59:52 +0000

    3.6-RC2-SNAPSHOT

--- Hazelcast.Examples/Properties/AssemblyInfo.cs
+++ Hazelcast.Examples/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.6.0.2")]
-[assembly: AssemblyFileVersion("3.6.0.2")]
-[assembly: AssemblyInformationalVersion("3.6.0.2")]
+[assembly: AssemblyVersion("3.6.0.3")]
+[assembly: AssemblyFileVersion("3.6.0.3")]
+[assembly: AssemblyInformationalVersion("3.6.0.3")]
