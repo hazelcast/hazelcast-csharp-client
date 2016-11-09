7799e2e6b235b2b1675f7fb10d9895e1a453bb9a
Asım Arslan <arslanasim@gmail.com>
2016-11-09 12:41:11 +0300

    map entry processor code sample

---
+++ Hazelcast.Examples/Map/MapEntryProcessorExample.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) 2008-2015, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Hazelcast.Client;
+using Hazelcast.Config;
+using Hazelcast.Core;
+using Hazelcast.Examples.Models;
+
+namespace Hazelcast.Examples.Map
+{
+    public class MapEntryProcessorExample
+    {
+        public static void Run(string[] args)
+        {
+            Environment.SetEnvironmentVariable("hazelcast.logging.level", "info");
+            Environment.SetEnvironmentVariable("hazelcast.logging.type", "console");
+
+            var config = new ClientConfig();
+
+            config.GetNetworkConfig().AddAddress("127.0.0.1");
+            config.GetSerializationConfig()
+                .AddDataSerializableFactory(EntryProcessorDataSerializableFactory.FactoryId,
+                    new EntryProcessorDataSerializableFactory());
+
+            var client = HazelcastClient.NewHazelcastClient(config);
+
+            var map = client.GetMap<int, string>("entry-processor-example");
+
+            Console.WriteLine("Populating map");
+
+            for (int i = 0; i < 10; i++)
+            {
+                map.Put(i, "value" + i);
+            }
+
+            Console.WriteLine("Map size: " + map.Size());
+
+            var result = map.ExecuteOnEntries(new UpdateEntryProcessor("value-UPDATED"), Predicates.Sql("this==value5"));
+
+            Console.WriteLine("Updated value result: " +result[5]);
+
+            Console.WriteLine("The same value from  the map: " + map.Get(5));
+
+            client.Shutdown();
+        }
+    }
+}
---
+++ Hazelcast.Examples/Models/EntryProcessor.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) 2008-2015, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Hazelcast.IO;
+using Hazelcast.IO.Serialization;
+using Hazelcast.Map;
+
+namespace Hazelcast.Examples.Models
+{
+
+    /// <summary>
+    /// Hazezelcast-test.jar has the same EntryProcessor to be used on server side
+    ///  named <c>com.hazelcast.client.test.IdentifiedEntryProcessor</c>
+    /// </summary>
+    public class UpdateEntryProcessor : IEntryProcessor, IIdentifiedDataSerializable
+    {
+        public const int ClassId = 1;
+
+        private string value;
+
+        public UpdateEntryProcessor(string value=null)
+        {
+            this.value = value;
+        }
+
+        public void ReadData(IObjectDataInput input)
+        {
+            value = input.ReadUTF();
+        }
+
+        public void WriteData(IObjectDataOutput output)
+        {
+            output.WriteUTF(value);
+        }
+
+        public int GetFactoryId()
+        {
+            return EntryProcessorDataSerializableFactory.FactoryId;
+        }
+
+        public int GetId()
+        {
+            return ClassId;
+        }
+    }
+
+
+    public class EntryProcessorDataSerializableFactory : IDataSerializableFactory
+    {
+        public const int FactoryId = 66;
+
+        public IIdentifiedDataSerializable Create(int typeId)
+        {
+            if(typeId == UpdateEntryProcessor.ClassId)
+            {
+                    return new UpdateEntryProcessor();
+            }
+            throw new InvalidOperationException("Unknown type id");
+        }
+    }
+}
