696a1a0c4dd773b22fcde4aa829b892aeee75ae8
Asım Arslan <arslanasim@gmail.com>
2016-11-11 18:44:09 +0300

    release prepare 3.7.1

--- Hazelcast.Examples/Properties/AssemblyInfo.cs
+++ Hazelcast.Examples/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("3.6.5.0")]
-[assembly: AssemblyFileVersion("3.6.5.0")]
-[assembly: AssemblyInformationalVersion("3.6.5.0")]
+[assembly: AssemblyVersion("3.7.1")]
+[assembly: AssemblyFileVersion("3.7.1")]
+[assembly: AssemblyInformationalVersion("3.7.1")]
