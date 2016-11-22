81d2f0e1125b7184e4d8a48ee7f10b2f988c35ec
Asım Arslan <arslanasim@gmail.com>
2016-11-22 12:39:02 +0300

    version 3.7.2-beta1

--- Hazelcast.Net/Properties/AssemblyInfo.cs
+++ Hazelcast.Net/Properties/AssemblyInfo.cs
@@ -16,9 +16,8 @@
 [assembly: System.Reflection.AssemblyConfiguration("Commit #####")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.CLSCompliant(true)]
-[assembly: System.Reflection.AssemblyVersion("3.7.2")]
-[assembly: System.Reflection.AssemblyFileVersion("3.7.2")]
-[assembly: System.Reflection.AssemblyInformationalVersion("3.7.2")]
+[assembly: System.Reflection.AssemblyVersion("3.7.2.*")]
+[assembly: System.Reflection.AssemblyInformationalVersion("3.7.2-beta1")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo(@"Hazelcast.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001004d81045a994968ac643918d7bbce405b2473471d8de6aed6bbffc0fe1874bfcabf3c0b437c6c5293a589bdcbe884c6d86934069b35deaf5ab2e770cbff41a20dd4014bb53e481c30bd3ead29437b02dec5916a717a4a2b4fd353e81238b89ae09e5ba0ab615c5fef7937aabab4e240c3dffe2b948047769eeb07f674589d0bb3")]
 
 
