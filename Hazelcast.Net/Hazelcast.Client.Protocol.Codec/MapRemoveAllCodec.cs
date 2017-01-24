d346b5937b29aafb2070c6769b778aa1789b1718
Asım Arslan <arslanasim@gmail.com>
2017-01-24 10:55:59 +0300

    Map RemoveAll with predicate support implementation.

---
+++ Hazelcast.Net/Hazelcast.Client.Protocol.Codec/MapRemoveAllCodec.cs
@@ -0,0 +1,68 @@
+// Copyright (c) 2008-2015, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Hazelcast.Client.Protocol.Util;
+using Hazelcast.IO.Serialization;
+
+namespace Hazelcast.Client.Protocol.Codec
+{
+    internal sealed class MapRemoveAllCodec
+    {
+        public static readonly MapMessageType RequestType = MapMessageType.MapRemoveAll;
+        public const int ResponseType = 100;
+        public const bool Retryable = false;
+
+        //************************ REQUEST *************************//
+
+        public class RequestParameters
+        {
+            public static readonly MapMessageType TYPE = RequestType;
+            public string name;
+            public IData predicate;
+
+            public static int CalculateDataSize(string name, IData predicate)
+            {
+                var dataSize = ClientMessage.HeaderSize;
+                dataSize += ParameterUtil.CalculateDataSize(name);
+                dataSize += ParameterUtil.CalculateDataSize(predicate);
+                return dataSize;
+            }
+        }
+
+        public static ClientMessage EncodeRequest(string name, IData predicate)
+        {
+            var requiredDataSize = RequestParameters.CalculateDataSize(name, predicate);
+            var clientMessage = ClientMessage.CreateForEncode(requiredDataSize);
+            clientMessage.SetMessageType((int) RequestType);
+            clientMessage.SetRetryable(Retryable);
+            clientMessage.Set(name);
+            clientMessage.Set(predicate);
+            clientMessage.UpdateFrameLength();
+            return clientMessage;
+        }
+
+        //************************ RESPONSE *************************//
+
+
+        public class ResponseParameters
+        {
+        }
+
+        public static ResponseParameters DecodeResponse(IClientMessage clientMessage)
+        {
+            var parameters = new ResponseParameters();
+            return parameters;
+        }
+    }
+}
