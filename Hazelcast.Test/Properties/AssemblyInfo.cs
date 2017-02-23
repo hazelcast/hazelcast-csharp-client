557f2531a0e099a5cd4dc72faf738a742f0f51a1
Asım Arslan <arslanasim@gmail.com>
2017-02-23 14:33:02 +0300

    fix AssemblyInfo and version

--- Hazelcast.Test/Properties/AssemblyInfo.cs
+++ Hazelcast.Test/Properties/AssemblyInfo.cs
@@ -17,10 +17,4 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Hazelcast .Net Client Test")]
 [assembly: AssemblyDescription("Hazelcast .Net Client Test")]
-[assembly: AssemblyCompany("Hazelcast Inc.")]
-[assembly: AssemblyProduct("Hazelcast Enterprise Edition")]
-[assembly: AssemblyCopyright("Copyright (c) 2008-2014, Hazelcast, Inc")]
-[assembly: ComVisible(false)]
 [assembly: Guid("6677ec55-89bb-4ec2-b85c-4e37f7a816e6")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
