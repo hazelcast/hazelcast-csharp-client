836cfe31bf7d562506204b0f7698737a8a2952b5
Asım Arslan <arslanasim@gmail.com>
2018-10-02 12:22:35 +0300

    Strong name signing build fixed

--- build-nuget.bat
+++ build-nuget.bat
@@ -2,4 +2,4 @@ nuget restore
 
 REM nuget.exe pack Hazelcast.Net/Hazelcast.Net.csproj -properties Configuration=Release
 
-msbuild Hazelcast.Net/Hazelcast.Net.csproj /p:Configuration=Release /target:Restore;Build;Pack
+msbuild Hazelcast.Net/Hazelcast.Net.csproj /p:Configuration=Release /p:PublicSign=false /p:AssemblyOriginatorKeyFile=../hazelcast.snk /target:Restore;Build;Pack
