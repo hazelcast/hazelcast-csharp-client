0c100d408c43e4ad5010e0c2e0095f1821e0bdca
Asım Arslan <arslanasim@gmail.com>
2018-12-04 16:35:50 +0300

    Ssl mutual authentication implementation

---
+++ Hazelcast.Test/Hazelcast.Client.Test/ClientSslTest.cs
@@ -0,0 +1,150 @@
+﻿// Copyright (c) 2008-2018, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using Hazelcast.Config;
+using Hazelcast.Core;
+using Hazelcast.Remote;
+using Hazelcast.Test;
+using NUnit.Framework;
+
+namespace Hazelcast.Client.Test
+{
+    [TestFixture]
+    [Category("enterprise")]
+    public class ClientSSLTest : ClientSSLBaseTest
+    {
+
+        [Test]
+        public void TestSSLEnabled_validateName_validName()
+        {
+            Setup(serverXml:Resources.hazelcast_ssl_signed,
+                isSslEnabled:true,
+                validateCertificateChain:true, 
+                validateCertificateName:true, 
+                checkCertificateRevocation:null, 
+                certSubjectName:ValidCertNameSigned, 
+                clientCertificate:null, 
+                certPassword:null);
+            Assert.True(Client.GetLifecycleService().IsRunning());
+        }
+
+        [Test]
+        public void TestSSLEnabled_validateName_invalidName()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {            
+                Setup(serverXml:Resources.hazelcast_ssl_signed, 
+                    isSslEnabled:true, 
+                    validateCertificateChain:true, 
+                    validateCertificateName:true, 
+                    checkCertificateRevocation:null, 
+                    certSubjectName:"Invalid Cert Name", 
+                    clientCertificate:null, 
+                    certPassword:null);
+            });
+        }
+
+        [Test]
+        public void TestSSLEnabled_validateChain_DoNotValidateName_invalidName()
+        {
+            Setup(serverXml:Resources.hazelcast_ssl_signed, 
+                isSslEnabled:true, 
+                validateCertificateChain:true, 
+                validateCertificateName:false, 
+                checkCertificateRevocation:null, 
+                certSubjectName:"Invalid Cert Name", 
+                clientCertificate:null, 
+                certPassword:null);
+            Assert.True(Client.GetLifecycleService().IsRunning());
+        }
+
+        [Test]
+        public void TestSSLEnabled_DoNotValidateChain_DoNotValidateName_invalidName()
+        {
+            Setup(serverXml:Resources.hazelcast_ssl_signed, 
+                isSslEnabled:true, 
+                validateCertificateChain:false, 
+                validateCertificateName:false, 
+                checkCertificateRevocation:null, 
+                certSubjectName:"Invalid Cert Name", 
+                clientCertificate:null, 
+                certPassword:null);
+            Assert.True(Client.GetLifecycleService().IsRunning());
+        }
+
+        [Test]
+        public void TestSSLDisabled()
+        {
+            Setup(serverXml:Resources.hazelcast, 
+                isSslEnabled:false, 
+                validateCertificateChain:null, 
+                validateCertificateName:null, 
+                checkCertificateRevocation:null, 
+                certSubjectName:null, 
+                clientCertificate:null, 
+                certPassword:null);
+            Assert.True(Client.GetLifecycleService().IsRunning());
+        }
+
+        [Test]
+        public void TestSSLEnabled_self_signed_remote_cert()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                Setup(serverXml:Resources.hazelcast_ssl, 
+                    isSslEnabled:true, 
+                    validateCertificateChain:null, 
+                    validateCertificateName:null, 
+                    checkCertificateRevocation:null, 
+                    certSubjectName:null, 
+                    clientCertificate:null, 
+                    certPassword:null);
+            });
+        }
+        
+        [Test]
+        public void TestSSLEnabled_signed_remote_cert()
+        {
+            Setup(serverXml:Resources.hazelcast_ssl_signed, 
+                isSslEnabled:true, 
+                validateCertificateChain:null, 
+                validateCertificateName:null, 
+                checkCertificateRevocation:null, 
+                certSubjectName:null, 
+                clientCertificate:null, 
+                certPassword:null);
+            Assert.True(Client.GetLifecycleService().IsRunning());
+        }
+
+        [Test]
+        public void TestSSLEnabled_validateChain_validateName_validName()
+        {
+            Setup(serverXml:Resources.hazelcast_ssl_signed, 
+                isSslEnabled:true, 
+                validateCertificateChain:null, 
+                validateCertificateName:true, 
+                checkCertificateRevocation:null, 
+                certSubjectName:ValidCertNameSigned, 
+                clientCertificate:null, 
+                certPassword:null);
+            Assert.True(Client.GetLifecycleService().IsRunning());
+        }
+    }
+}
---
+++ Hazelcast.Test/Properties/Resources.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace Hazelcast.Test
+{
+    internal static class Resources
+    {
+        //Binary files
+        public static byte[] client1 {get { return GetBytes("Resources.client1.pfx"); }}
+        public static byte[] client2 {get { return GetBytes("Resources.client2.pfx"); }}
+
+        //Text files
+        public static string hazelcast_config_full {get { return GetXmlResourceContent("hazelcast-client-full"); }}
+        public static string hazelcast {get { return GetXmlResourceContent("hazelcast"); }}
+        public static string hazelcast_delay {get { return GetXmlResourceContent("hazelcast-delay"); }}
+        public static string hazelcast_hb {get { return GetXmlResourceContent("hazelcast-hb"); }}
+        public static string hazelcast_ipv6 {get { return GetXmlResourceContent("hazelcast-ipv6"); }}
+        public static string hazelcast_ma_required {get { return GetXmlResourceContent("hazelcast-ma-required"); }}
+        public static string hazelcast_ma_optional {get { return GetXmlResourceContent("hazelcast-ma-optional"); }}
+        public static string hazelcast_nearcache {get { return GetXmlResourceContent("hazelcast-nearcache"); }}
+        public static string hazelcast_ssl_signed {get { return GetXmlResourceContent("hazelcast-ssl-signed"); }}
+        public static string hazelcast_ssl {get { return GetXmlResourceContent("hazelcast-ssl"); }}
+        public static string hazelcast_stat {get { return GetXmlResourceContent("hazelcast-stat"); }}
+
+        private static byte[] GetBytes(string name)
+        {
+            var assembly = typeof(Resources).Assembly;
+            using (var stream = assembly.GetManifestResourceStream(name))
+            {
+                if (stream == null)
+                {
+                    return null;
+                }
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+        private static string GetXmlResourceContent(string name)
+        {
+            var fullName = "Resources." + name + ".xml";
+            var bytes = GetBytes(fullName);
+            return Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
