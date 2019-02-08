3282d82e4080d574ad63756c604c64f947acb9d9
Alek Stankiewicz <stankiewiczal@gmail.com>
2019-02-08 16:19:51 +0100

    Refactored usage of volatile member

--- Hazelcast.Net/Hazelcast.Client.Proxy/ClientPNCounterProxy.cs
+++ Hazelcast.Net/Hazelcast.Client.Proxy/ClientPNCounterProxy.cs
@@ -357,12 +357,13 @@ namespace Hazelcast.Client.Proxy
 
             while (true)
             {
-                if (_observedClock.IsAfter(newVectorClock))
-                    break;
-
                 // Store the original value just to avoid issue with data capture order
                 var originalValue = _observedClock;
-                if (Interlocked.CompareExchange(ref _observedClock, newVectorClock, _observedClock) == originalValue)
+
+                if (originalValue.IsAfter(newVectorClock))
+                    break;
+                
+                if (Interlocked.CompareExchange(ref _observedClock, newVectorClock, originalValue) == originalValue)
                     break;
             }
         }
