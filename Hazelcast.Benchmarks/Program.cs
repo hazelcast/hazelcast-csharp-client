0f2b38b39a941089faea90d9e7976ee0793f2838
Szymon Kulec <scooletz@gmail.com>
2019-07-01 17:12:59 +0200

    Initial benchmarks program

---
+++ Hazelcast.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Hazelcast.Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+    }
+}
