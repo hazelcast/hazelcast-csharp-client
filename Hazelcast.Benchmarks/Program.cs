83ec71868f9d8bbbe804b21fb2c197ccee066669
Szymon Kulec <scooletz@gmail.com>
2019-08-27 13:02:25 +0200

    Benchmarks added

---
+++ Hazelcast.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Hazelcast.Benchmarks
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
