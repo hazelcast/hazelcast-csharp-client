f48fccd0f1483342955dfbdf714e825b12888670
Stephan <stephane@umbraco.dk>
2020-06-19 11:30:55 +0200

    wipe solution

--- build-nuget.bat
+++
@@ -1,5 +0,0 @@
-nuget restore
-
-REM nuget.exe pack Hazelcast.Net/Hazelcast.Net.csproj -properties Configuration=Release
-
-msbuild Hazelcast.Net/Hazelcast.Net.csproj /p:Configuration=Release /p:PublicSign=false /p:AssemblyOriginatorKeyFile=../hazelcast.snk /target:Restore;Build;Pack
