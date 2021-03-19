ff36e69907134f34658628af08a71d80fc17132f
Stephan <stephane.gay@hazelcast.com>
2021-03-19 17:37:43 +0100

    Metrics

---
+++ src/Hazelcast.Net/Metrics/MetricsCompressor.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using Hazelcast.Core;
+
+namespace Hazelcast.Metrics
+{
+    // provides Java DataOutputStream (at least, what we really need) to C#
+
+    // compresses metrics, closely following the MetricsCompressor.java code to ensure interoperability
+    internal class MetricsCompressor : IDisposable
+    {
+        private const int InitialStringsBufferSize = 2 << 10; // 2kB
+        private const int InitialMetricsBufferSize = 2 << 11; // 4kB
+        private const int InitialTempBufferSize = 2 << 8; // 512B
+
+        // output streams for the blob containing the strings
+        private MemoryStream _stringsBuffer;
+        private DeflateStream _stringsDeflateStream;
+        private DataOutputStream _stringsOutput;
+
+        // output streams for the blob containing the metrics
+        private MemoryStream _metricsBuffer;
+        private DeflateStream _metricsDeflateStream;
+        private DataOutputStream _metricsOutput;
+
+        // temporary buffer to avoid fragmented writes to the deflate streams, when
+        // when writing primitive fields - TODO: is this needed in C#?
+        private readonly MemoryStream _tempBuffer;
+        private readonly DataOutputStream _tempOutput;
+
+        private SortedDictionary<string, int> _strings = new SortedDictionary<string, int>();
+        private int _count;
+        private MetricDescriptor _lastDescriptor;
+        private bool _disposed, _closed;
+
+        public MetricsCompressor()
+        {
+            Reset(InitialStringsBufferSize, InitialMetricsBufferSize);
+
+            // that one is never reset
+            _tempBuffer = new MemoryStream(InitialTempBufferSize);
+            _tempOutput = new DataOutputStream(_tempBuffer);
+        }
+
+        private static void Reset(ref MemoryStream buffer, ref DeflateStream deflate, ref DataOutputStream output, int size)
+        {
+            deflate?.Dispose();
+
+            // shrink if capacity is more than 50% larger than the estimated size
+            if (buffer == null || buffer.Capacity > 3 * size/ 2)
+            {
+                buffer?.Dispose();
+                buffer = new MemoryStream(size);
+            }
+
+            buffer.Seek(0, SeekOrigin.Begin);
+            deflate = new DeflateStream(buffer, CompressionLevel.Fastest, true);
+            output = new DataOutputStream(deflate);
+        }
+
+        private void Reset(int stringsBufferSize, int metricsBufferSize)
+        {
+            Reset(ref _stringsBuffer, ref _stringsDeflateStream, ref _stringsOutput, stringsBufferSize);
+            Reset(ref _metricsBuffer, ref _metricsDeflateStream, ref _metricsOutput, metricsBufferSize);
+
+            _strings = new SortedDictionary<string, int>();
+            _count = 0;
+            _lastDescriptor = null;
+            _closed = false;
+        }
+
+        public void Dispose()
+        {
+            if (_disposed) return;
+            _disposed = true;
+
+            _tempBuffer?.Dispose();
+
+            _stringsBuffer?.Dispose();
+            _stringsDeflateStream?.Dispose();
+
+            _metricsBuffer?.Dispose();
+            _metricsDeflateStream?.Dispose();
+        }
+
+        public void AppendLong(MetricDescriptor descriptor, long value)
+        {
+            if (_closed) throw new InvalidOperationException("Compressor is closed.");
+
+            _tempBuffer.Seek(0, SeekOrigin.Begin);
+            AppendDescriptor(descriptor);
+            _tempOutput.WriteByte((byte) MetricValueType.Long);
+            _tempOutput.WriteLong(value);
+
+            // protect case
+            if (_tempBuffer.Length > int.MaxValue) throw new InvalidOperationException($"Out of range: _tempBuffer.Length ({_tempBuffer.Length}).");
+
+            _metricsOutput.Write(_tempBuffer.GetBuffer(), 0, (int) _tempBuffer.Length);
+        }
+
+        public void AppendDouble(MetricDescriptor descriptor, double value)
+        {
+            if (_closed) throw new InvalidOperationException("Compressor is closed.");
+
+            _tempBuffer.Seek(0, SeekOrigin.Begin);
+            AppendDescriptor(descriptor);
+            _tempOutput.WriteByte((byte) MetricValueType.Double);
+            _tempOutput.WriteDouble(value);
+
+            // protect case
+            if (_tempBuffer.Length > int.MaxValue) throw new InvalidOperationException($"Out of range: _tempBuffer.Length ({_tempBuffer.Length}).");
+
+            _metricsOutput.Write(_tempBuffer.GetBuffer(), 0, (int) _tempBuffer.Length);
+        }
+
+        private void AppendDescriptor(MetricDescriptor descriptor)
+        {
+            var mask = GetMask(descriptor);
+            _tempOutput.WriteByte((byte) mask);
+
+            if (mask.HasNone(DescriptorMask.Prefix))
+                _tempOutput.WriteInt(GetStringId(descriptor.Prefix));
+
+            if (mask.HasNone(DescriptorMask.Name))
+                _tempOutput.WriteInt(GetStringId(descriptor.Name));
+
+            if (mask.HasNone(DescriptorMask.DiscriminatorName))
+                _tempOutput.WriteInt(GetStringId(descriptor.DiscriminatorName));
+
+            if (mask.HasNone(DescriptorMask.DiscriminatorValue))
+                _tempOutput.WriteInt(GetStringId(descriptor.DiscriminatorValue));
+
+            if (mask.HasNone(DescriptorMask.Unit))
+                _tempOutput.WriteInt((int) descriptor.Unit);
+
+            //if (mask.HasNone(DescriptorMask.ExcludedTargets))
+            //    tmpDos.WriteByte(MetricTarget.BitSet(descriptor.ExcludedTargets));
+
+            if (mask.HasNone(DescriptorMask.TagCount))
+                _tempOutput.WriteByte((byte) descriptor.Tags.Count);
+
+            // further compression would be possible by writing only the different tags
+            foreach (var (tagName, tagValue) in descriptor.Tags)
+            {
+                _tempOutput.WriteInt(GetStringId(tagName));
+                _tempOutput.WriteInt(GetStringId(tagValue));
+            }
+
+            _count++;
+            _lastDescriptor = descriptor; // TODO: Java clones the descriptor, are we safe?
+        }
+
+        private DescriptorMask GetMask(MetricDescriptor descriptor)
+        {
+            var mask = DescriptorMask.None;
+
+            if (_lastDescriptor == null) 
+                return mask;
+
+            if (descriptor.Prefix == _lastDescriptor.Prefix)
+                mask |= DescriptorMask.Prefix;
+
+            if (descriptor.Name == _lastDescriptor.Name)
+                mask |= DescriptorMask.Name;
+
+            if (descriptor.DiscriminatorName == _lastDescriptor.DiscriminatorName)
+                mask |= DescriptorMask.DiscriminatorName;
+
+            if (descriptor.DiscriminatorValue == _lastDescriptor.DiscriminatorValue)
+                mask |= DescriptorMask.DiscriminatorValue;
+
+            if (descriptor.Unit == _lastDescriptor.Unit)
+                mask |= DescriptorMask.Unit;
+
+            //if (Objects.equals(descriptor.excludedTargets(), lastDescriptor.excludedTargets()))
+            mask |= DescriptorMask.ExcludedTargets;
+
+            if (descriptor.Tags.Count == _lastDescriptor.Tags.Count)
+                mask |= DescriptorMask.TagCount;
+
+            return mask;
+        }
+
+        private int GetStringId(string s)
+        {
+            if (s == null)
+                return -1;
+
+            if (_strings.TryGetValue(s, out var id)) 
+                return id;
+
+            id = _strings.Count;
+            _strings[s] = id;
+            return id;
+        }
+
+        private void AppendStrings()
+        {
+            _stringsOutput.WriteInt(_strings.Count);
+            var prevText = "";
+
+            // sorted dictionary is ordered by natural order of its keys
+            foreach (var (stringText, stringId) in _strings)
+            {
+                // this should have been checked earlier, this is a safety check
+                // this protects the length casts to byte below
+                if (stringText.Length > byte.MaxValue)
+                    throw new InvalidOperationException($"Out of range: stringText.Length (\"{stringText}\").");
+
+                // find the span of chars that is common to stringText and prevText
+                var maxCommonLen = Math.Min(prevText.Length, stringText.Length);
+                var commonLen = 0;
+                while (commonLen < maxCommonLen && stringText[commonLen] == prevText[commonLen])
+                    commonLen++;
+
+                // compute the length of remaining, non-common chars
+                var diffLen = stringText.Length - commonLen;
+
+                // write through temp buffer
+                _tempBuffer.Seek(0, SeekOrigin.Begin);
+
+                // write
+                _tempOutput.WriteInt(stringId);
+                _tempOutput.WriteByte((byte) commonLen);
+                _tempOutput.WriteByte((byte) diffLen);
+                _tempOutput.WriteString(stringText, commonLen, diffLen);
+
+                // protect case
+                if (_tempBuffer.Length > int.MaxValue) throw new InvalidOperationException($"Out of range: _tempBuffer.Length ({_tempBuffer.Length}).");
+
+                _stringsOutput.Write(_tempBuffer.GetBuffer(), 0, (int) _tempBuffer.Length);
+
+                prevText = stringText;
+            }
+        }
+
+        private byte[] GetBytes()
+        {
+            const int binaryFormatVersion = 1;
+            const int sizeVersion = 2;
+            const int sizeDictionaryBlob = 4;
+            const int sizeCountMetrics = 4;
+
+            const int bitsInByte = 8;
+            const int byteMask = 0xff;
+
+            // close the compressor, we're going to dispose the deflate streams
+            _closed = true;
+
+            AppendStrings();
+
+            // got to dispose the deflate streams to flush them all
+            _stringsDeflateStream.Dispose();
+            _stringsDeflateStream = null;
+            _metricsDeflateStream.Dispose();
+            _metricsDeflateStream = null;
+
+            // version info + dictionary length + dictionary blob + number of metrics + metrics blob
+            var completeSize = sizeVersion + sizeDictionaryBlob + _stringsBuffer.Length + sizeCountMetrics + _metricsBuffer.Length;
+
+            // protect casts
+            if (completeSize > int.MaxValue) throw new InvalidOperationException($"Out of range: completeSize ({completeSize}).");
+            if (_stringsBuffer.Length > int.MaxValue) throw new InvalidOperationException($"Out of range: _stringsBuffer.Length ({_stringsBuffer.Length}).");
+            if (_metricsBuffer.Length > int.MaxValue) throw new InvalidOperationException($"Out of range: _metricsBuffer.Length ({_metricsBuffer.Length}).");
+
+            using var buffer = new MemoryStream((int) completeSize);
+            var output = new DataOutputStream(buffer);
+
+            // ReSharper disable once ShiftExpressionResultEqualsZero - well, yes
+            output.WriteByte((binaryFormatVersion >> bitsInByte) & byteMask);
+            output.WriteByte(binaryFormatVersion & byteMask);
+
+            output.WriteInt((int) _stringsBuffer.Length);
+            output.Write(_stringsBuffer.GetBuffer(), 0, (int) _stringsBuffer.Length);
+            output.WriteInt(_count);
+            output.Write(_metricsBuffer.GetBuffer(), 0, (int) _metricsBuffer.Length);
+
+            return buffer.ToArray();
+        }
+
+        public byte[] GetBytesAndReset()
+        {
+            var bytes = GetBytes();
+
+            const int sizeFactoryNumerator = 11;
+            const int sizeFactoryDenominator = 10;
+
+            var dictionaryBufferSize = (int) _stringsBuffer.Length * sizeFactoryNumerator / sizeFactoryDenominator;
+            var metricsBufferSize = (int) _metricsBuffer.Length * sizeFactoryNumerator / sizeFactoryDenominator;
+            Reset(dictionaryBufferSize, metricsBufferSize);
+
+            return bytes;
+        }
+
+        [Flags]
+        private enum DescriptorMask : byte
+        {
+            None = 0,
+            Prefix = 1,
+            Name = 1 << 1,
+            DiscriminatorName = 1 << 2,
+            DiscriminatorValue = 1 << 3,
+            Unit = 1 << 4,
+            ExcludedTargets = 1 << 5,
+            TagCount = 1 << 6
+        }
+    }
+}
---
+++ src/Hazelcast.Net/Metrics/MetricsPublisher.cs
@@ -0,0 +1,168 @@
+﻿// Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Hazelcast.Clustering;
+using Hazelcast.Core;
+using Hazelcast.NearCaching;
+using Hazelcast.Protocol.Codecs;
+using Microsoft.Extensions.Logging;
+
+namespace Hazelcast.Metrics
+{
+    internal class MetricsPublisher : IAsyncDisposable
+    {
+        private readonly Cluster _cluster;
+        private readonly NearCacheManager _nearCaches;
+        private readonly MetricsOptions _options;
+        private readonly ILogger _logger;
+
+        private readonly CancellationTokenSource _cancel;
+        private readonly Task _publishing;
+
+        public MetricsPublisher(Cluster cluster, NearCacheManager nearCaches, MetricsOptions options, ILoggerFactory loggerFactory)
+        {
+            _cluster = cluster;
+            _nearCaches = nearCaches;
+            _options = options;
+            _logger = loggerFactory.CreateLogger<MetricsPublisher>();
+
+            // start the task
+            _cancel = new CancellationTokenSource();
+            _publishing = PublishAsync(_cancel.Token);
+
+            _logger.LogDebug($"Publishing metrics every {_options.PeriodSeconds}s");
+        }
+
+        private async Task PublishAsync(CancellationToken cancellationToken)
+        {
+            var delay = TimeSpan.FromSeconds(_options.PeriodSeconds);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(delay, cancellationToken).CfAwait();
+
+                try
+                {
+                    await SendMetricsAsync(cancellationToken).CfAwait();
+                }
+                catch (Exception e)
+                {
+                    // should never happen as SendMetricsAsync should not throw - but better be safe
+                    _logger.LogError(e, "Caught exception in MetricsPublished.");
+                }
+            }
+        }
+
+        private async Task SendMetricsAsync(CancellationToken cancellationToken)
+        {
+            // the Java client gets these from a random connection
+            // we try to be more consistent and always pick the oldest active connection
+            var connection = _cluster.Members.GetOldestConnection();
+            if (connection == null)
+            {
+                _logger.LogDebug("Cannot send metrics, client is not connected.");
+                return;
+            }
+
+            var timestamp = Clock.Milliseconds;
+            var stats = new List<IStat>();
+
+            // these are the stats currently sent by the Java v4 client
+
+            // FIXME how are "gauge" working in Java?!
+
+            stats.AddStat("lastStatisticsCollectionTime", timestamp);
+            stats.AddStat("enterprise", false);
+            stats.AddStat("clientType", "CSHARP");
+            stats.AddStat("clientVersion", ClientVersion.Version);
+            stats.AddStat("clientName", _cluster.ClientName);
+
+            stats.AddStat("clusterConnectionTimestamp", Clock.ToEpoch(connection.ConnectTime.UtcDateTime)); // TODO: ToEpoch supports DateTimeOffset
+            stats.AddStat("clientAddress", connection.LocalEndPoint.Address);
+            stats.AddStat("credentials.principal", connection.Principal);
+
+            // these are the os values copied from v3, TODO: can we do better?
+            stats.AddStat("os.committedVirtualMemorySize", () => Process.GetCurrentProcess().VirtualMemorySize64);
+            stats.AddEmptyStat("os.freePhysicalMemorySize");
+            stats.AddEmptyStat("os.freeSwapSpaceSize");
+            stats.AddEmptyStat("os.maxFileDescriptorCount");
+            stats.AddStat("os.openFileDescriptorCount", () => Process.GetCurrentProcess().HandleCount);
+            stats.AddStat("os.processCpuTime", () => (long) Process.GetCurrentProcess().TotalProcessorTime.TotalMilliseconds * 1000000);
+            stats.AddEmptyStat("os.systemLoadAverage"); // double value, everything else is long
+            stats.AddEmptyStat("os.totalPhysicalMemorySize");
+            stats.AddEmptyStat("os.totalSwapSpaceSize");
+
+            // these are the runtime values copied from v3, TODO: can we do better?
+            stats.AddStat("runtime.availableProcessors", () => Environment.ProcessorCount);
+            stats.AddEmptyStat("runtime.freeMemory");
+            stats.AddStat("runtime.maxMemory", () => Process.GetCurrentProcess().MaxWorkingSet.ToInt64());
+            stats.AddStat("runtime.totalMemory", () => Process.GetCurrentProcess().WorkingSet64);
+            stats.AddStat("runtime.uptime", () => (long) (DateTime.Now - Process.GetCurrentProcess().StartTime).TotalMilliseconds);
+            stats.AddStat("runtime.usedMemory", () => Process.GetCurrentProcess().WorkingSet64);
+
+            // this was in v3, not in Java v4
+            //stats.AddEmptyStat("executionService.userExecutorQueueSize");
+
+            await foreach (var nearCache in _nearCaches)
+                stats.AddStats(nearCache);
+
+            //metricsRegistry.Collect(metricsCollector);
+            //var metrics = metricsCollector.GetBytes();
+
+            var metrics = Array.Empty<byte>();
+
+            if (cancellationToken.IsCancellationRequested) return; // last chance to cancel
+
+            try
+            {
+                // non-cancelable
+                var sstats = stats.Serialize();
+                _logger.LogDebug("Send stats: " + sstats);
+                await SendMetricsAsync(timestamp, sstats, metrics).CfAwait();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Failed to send metrics.");
+            }
+        }
+
+        private async Task SendMetricsAsync(long timestamp, string attributes, byte[] metrics)
+        {
+            if (!_cluster.IsConnected) // last chance to avoid an exception
+            {
+                _logger.LogDebug("Cannot send metrics, client is not connected.");
+                return;
+            }
+
+            _logger.LogDebug("Send metrics.");
+
+            var requestMessage = ClientStatisticsCodec.EncodeRequest(timestamp, attributes, metrics);
+            var responseMessage = await _cluster.Messaging.SendAsync(requestMessage).CfAwait();
+            var _ = ClientStatisticsCodec.DecodeResponse(responseMessage);
+        }
+
+        /// <inheritdoc />
+        public async ValueTask DisposeAsync()
+        {
+            _cancel.Cancel();
+            await _publishing.CfAwaitCanceled();
+            _cancel.Dispose();
+        }
+    }
+}
