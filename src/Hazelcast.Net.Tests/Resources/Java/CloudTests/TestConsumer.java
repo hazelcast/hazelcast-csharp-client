61ef6f8318e43776f1cbc124e9d65099e757ea5f
Stephan <stephane.gay@hazelcast.com>
2021-03-19 17:38:24 +0100

    Cloud tests

---
+++ src/Hazelcast.Net.Tests/Resources/Java/CloudTests/TestConsumer.java
@@ -0,0 +1,13 @@
+﻿import com.hazelcast.internal.metrics.MetricConsumer;
+import com.hazelcast.internal.metrics.MetricDescriptor;
+
+public class TestConsumer implements MetricConsumer {
+
+    public void consumeLong(MetricDescriptor descriptor, long value) {
+        System.out.println(String.format("%s = %d", descriptor.metric(), value));
+    }
+
+    public void consumeDouble(MetricDescriptor descriptor, double value) {
+        System.out.println(String.format("%s = %f", descriptor.metric(), value));
+    }
+}
