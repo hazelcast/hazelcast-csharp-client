a3d8df66aef81d15912ed199741cea2e1b8f0f5c
Oleksii Bespalov <alexb5dh@gmail.com>
2021-04-24 01:32:43 +0300

    `AtomicReference` basic documentation and example

---
+++ doc/dev/doc/atomicref.md
@@ -0,0 +1,35 @@
+# AtomicReference
+
+> [!NOTE]
+> IAtomicReference is a member of CP Subsystem API. For detailed information, see the [CP SubSystem documentation](cpsubsystem.md).
+
+Hazelcast @Hazelcast.CP.IAtomicReference is the distributed implementation of `java.util.concurrent.atomic.AtomicReference` and offers most of its operations such as Get, Set, GetAndSet and CompareAndSet. You can also think of GetAndSet, CompareAndSet as @System.Interlocked Exchange and CompareExchange methods for distributed values. Since @Hazelcast.CP.IAtomicReference is a distributed implementation, these operations involve remote calls and thus their performances differ from local, in-memory, references.
+
+The following example code adds a dot the end of any string added to the shared reference:
+
+```csharp
+var cancellationSource = new CancellationTokenSource();
+var cancellationToken = cancellationSource.Token;
+
+await using var client = await HazelcastClientFactory.StartNewClientAsync(options);
+await using var sentence = await client.CPSubsystem.GetAtomicReferenceAsync<string>("sentence-unique-key");
+
+while (!cancellationToken.IsCancellationRequested)
+{
+    var value = await sentence.GetAsync();
+
+    if (value != null && !value.EndsWith("."))
+    {
+        var newValue = value + ".";
+
+        if (!await sentence.CompareAndSetAsync(value, newValue))
+            continue;
+    }
+
+    await Task.Delay(100, cancellationToken);
+}
+
+await sentence.DestroyAsync();
+```
+
+Note that sending functions to, and executing functions on, `AtomicReference` as documented for the Java client (see [this page](https://docs.hazelcast.com/imdg/latest/data-structures/iatomicreference.html)) are not supported by the C# client.
---
+++ src/Hazelcast.Net.Examples/CP/AtomicReferenceExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// ReSharper disable LocalizableElement
+
+namespace Hazelcast.Examples.CP
+{
+    public class AtomicReferenceExample
+    {
+        public static async Task Main(string[] args)
+        {
+            var options = new HazelcastOptionsBuilder()
+                .With(args)
+                .WithConsoleLogger()
+                .Build();
+
+            // create a Hazelcast client and connect to a server running on localhost
+            await using var client = await HazelcastClientFactory.StartNewClientAsync(options);
+
+            // get the CP AtomicRef from the cluster
+            await using var aref = await client.CPSubsystem.GetAtomicReferenceAsync<string>($"atomicref-example-{Guid.NewGuid()}");
+            Console.WriteLine($"Initial value: {await aref.GetAsync()}, is default: {await aref.IsNullAsync()}");
+
+            await aref.SetAsync(RandomString());
+            Console.WriteLine($"Value after set: {await aref.GetAsync()}, is default: {await aref.IsNullAsync()}");
+
+            var previous = await aref.GetAndSetAsync(RandomString());
+            Console.WriteLine($"Value after get-and-set: {await aref.GetAsync()}, previous: {previous}");
+
+            previous = await aref.GetAsync();
+            await aref.CompareAndSetAsync(previous, RandomString());
+            Console.WriteLine($"Value after compare-and-set: {await aref.GetAsync()}, previous: {previous}");
+
+            await aref.ClearAsync();
+            Console.WriteLine($"Value after clear: {await aref.GetAsync()}, is default: {await aref.IsNullAsync()}");
+
+            // destroy the AtomicLong
+            await aref.DestroyAsync();
+        }
+
+        private static string RandomString() => Guid.NewGuid().ToString().Substring(0, 8);
+    }
+}
