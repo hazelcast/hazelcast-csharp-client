89ed8bbcc383ef4d0c6f36d17b80f1b687ff4000
Stephan <stephane.gay@hazelcast.com>
2022-04-12 14:10:15 +0200

    Compact - Schemas (#697)

---
+++ src/Hazelcast.Net.Tests/Serialization/Compact/SchemasLocalTests.cs
@@ -0,0 +1,312 @@
+﻿// Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Hazelcast.Core;
+using Hazelcast.Messaging;
+using Hazelcast.Protocol.Codecs;
+using Hazelcast.Serialization;
+using Hazelcast.Serialization.Compact;
+using Hazelcast.Testing;
+using NUnit.Framework;
+
+namespace Hazelcast.Tests.Serialization.Compact
+{
+    [TestFixture]
+    public class SchemasLocalTests
+    {
+        private class ClusterMessagingStub : IClusterMessaging
+        {
+            private readonly Func<ClientMessage, Task<ClientMessage>> _respond;
+
+            public ClusterMessagingStub(Func<ClientMessage, Task<ClientMessage>> respond)
+            {
+                _respond = respond;
+            }
+
+            public Func<ValueTask> SendingMessage { get; set; }
+
+            public Task<ClientMessage> SendAsync(ClientMessage requestMessage, bool triggerEvents, CancellationToken cancellationToken)
+            {
+                return _respond(requestMessage);
+            }
+
+            public async Task<ClientMessage> SendAsync(ClientMessage requestMessage, CancellationToken cancellationToken)
+            {
+                await SendingMessage.AwaitEach().CfAwait();
+                return await _respond(requestMessage);
+            }
+        }
+
+        private static Schema GetSchema() => GetSchema("typename", "fieldname");
+
+        private static Schema GetSchema(string typename, string fieldname) => new Schema(typename, new[]
+        {
+            new SchemaField(fieldname, FieldKind.String)
+        });
+
+        [Test]
+        public void CanDispose()
+        {
+            using var schemas = new Schemas(new ClusterMessagingStub(_ => throw new NotImplementedException()));
+        }
+
+        [Test]
+        public void CanAddAndThenGetSchema()
+        {
+            var schemas = new Schemas(new ClusterMessagingStub(_ => throw new NotImplementedException()));
+            var schema = GetSchema();
+
+            Assert.That(schemas.TryGet(schema.Id, out _), Is.False);
+            schemas.Add(schema, false);
+            Assert.That(schemas.TryGet(schema.Id, out var returned), Is.True);
+            Assert.That(returned.Id, Is.EqualTo(schema.Id));
+        }
+
+        [Test]
+        public async Task MissingSchemaIsFetchedAndCached()
+        {
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(requestMessage =>
+            {
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientFetchSchemaCodec.RequestMessageType));
+                Interlocked.Increment(ref count);
+                return Task.FromResult(ClientFetchSchemaServerCodec.EncodeResponse(GetSchema()));
+            }));
+            var schema = GetSchema();
+
+            Assert.That(schemas.TryGet(schema.Id, out _), Is.False);
+
+            Assert.That(count, Is.EqualTo(0)); // no message has been responded yet
+
+            var fetched = await schemas.GetOrFetchAsync(schema.Id).ConfigureAwait(false);
+            Assert.That(count, Is.EqualTo(1)); // a ClientFetchSchemaCodec message was responded
+            Assert.That(fetched, Is.Not.Null); // and returned a schema
+            Assert.That(fetched.Id, Is.EqualTo(schema.Id)); // which is the right one
+
+            _ = await schemas.GetOrFetchAsync(schema.Id).ConfigureAwait(false);
+            Assert.That(count, Is.EqualTo(1)); // no additional message was responded, the schema was cached
+        }
+
+        [Test]
+        public async Task UnknowSchemaIsFetchedAgain()
+        {
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(requestMessage =>
+            {
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientFetchSchemaCodec.RequestMessageType));
+                Interlocked.Increment(ref count);
+                return Task.FromResult(ClientFetchSchemaServerCodec.EncodeResponse(null));
+            }));
+
+            Assert.That(count, Is.EqualTo(0)); // no message has been responded yet
+
+            var fetched = await schemas.GetOrFetchAsync(42).ConfigureAwait(false);
+            Assert.That(count, Is.EqualTo(1)); // a ClientFetchSchemaCodec message was responded
+            Assert.That(fetched, Is.Null); // and returned no schema
+
+            fetched = await schemas.GetOrFetchAsync(42).ConfigureAwait(false);
+            Assert.That(count, Is.EqualTo(2)); // a ClientFetchSchemaCodec message was responded, again
+            Assert.That(fetched, Is.Null); // and returned no schema
+        }
+
+        [Test]
+        public async Task AddedSingleSchemaWantsToBePublishedOnce()
+        {
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(requestMessage =>
+            {
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientSendSchemaCodec.RequestMessageType));
+                Interlocked.Increment(ref count);
+                return Task.FromResult(ClientSendSchemaServerCodec.EncodeResponse());
+            }));
+
+            var schema = GetSchema();
+
+            Assert.That(schemas.TryGet(schema.Id, out _), Is.False);
+            schemas.Add(schema, false);
+
+            Assert.That(count, Is.EqualTo(0)); // no message has been responded yet
+
+            await schemas.PublishAsync().CfAwait();
+            Assert.That(count, Is.EqualTo(1)); // a ClientSendSchemaCodec message was responded
+
+            await schemas.PublishAsync().CfAwait();
+            Assert.That(count, Is.EqualTo(1)); // no additional message was responded
+        }
+
+        [Test]
+        public async Task AddedMultipleSchemasWantToBePublishedOnce()
+        {
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(requestMessage =>
+            {
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientSendAllSchemasCodec.RequestMessageType));
+                Interlocked.Increment(ref count);
+                return Task.FromResult(ClientSendAllSchemasServerCodec.EncodeResponse());
+            }));
+
+            var schema0 = GetSchema("typename", "fieldname0");
+            Assert.That(schemas.TryGet(schema0.Id, out _), Is.False);
+            schemas.Add(schema0, false);
+
+            var schema1 = GetSchema("typename", "fieldname1");
+            Assert.That(schemas.TryGet(schema1.Id, out _), Is.False);
+            schemas.Add(schema1, false);
+
+            Assert.That(count, Is.EqualTo(0)); // no message has been responded yet
+
+            await schemas.PublishAsync().CfAwait();
+            Assert.That(count, Is.EqualTo(1)); // a ClientSendAllSchemasCodec message was responded
+
+            await schemas.PublishAsync().CfAwait();
+            Assert.That(count, Is.EqualTo(1)); // no additional message was responded
+        }
+
+        [Test]
+        public async Task CanAddSchemaAsClusterSchema()
+        {
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(requestMessage =>
+            {
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientSendAllSchemasCodec.RequestMessageType));
+                Interlocked.Increment(ref count);
+                return Task.FromResult(ClientSendAllSchemasServerCodec.EncodeResponse());
+            }));
+
+            var schema = GetSchema("typename", "fieldname0");
+            Assert.That(schemas.TryGet(schema.Id, out _), Is.False);
+            schemas.Add(schema, true);
+
+            Assert.That(count, Is.EqualTo(0)); // no message has been responded yet
+
+            await schemas.PublishAsync().CfAwait();
+            Assert.That(count, Is.EqualTo(0)); // still, no message
+        }
+
+        // test the first 'if (_toPublish.Count == 0)' block in Schemas.PublishAsyncReallyAsync
+        //
+        [Test]
+        public async Task ConcurrentPublish1()
+        {
+            var semaphore = new SemaphoreSlim(0, 1);
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(async requestMessage =>
+            {
+                Interlocked.Increment(ref count);
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientSendSchemaCodec.RequestMessageType));
+                await semaphore.WaitAsync();
+                return ClientSendAllSchemasServerCodec.EncodeResponse();
+            }));
+
+            // add a schema that needs to be pushed to the cluster
+            var schema = GetSchema("typename", "fieldname0");
+            schemas.Add(schema, false);
+
+            // capture the schema, prepare the message, etc
+            var task0 = schemas.PublishAsync();
+
+            // capture the schema, wait for task0
+            var task1 = schemas.PublishAsync();
+
+            // unlock task0
+            semaphore.Release();
+
+            await task0; // send schema
+            await task1; // going to do nothing - nothing left to publish
+
+            Assert.That(count, Is.EqualTo(1));
+        }
+
+        // test the second 'if (_toPublish.Count == 0)' block in Schemas.PublishAsyncReallyAsync
+        //
+        [Test]
+        public async Task ConcurrentPublish2()
+        {
+            var semaphore = new SemaphoreSlim(0, 1);
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(async requestMessage =>
+            {
+                Interlocked.Increment(ref count);
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientSendSchemaCodec.RequestMessageType));
+                await semaphore.WaitAsync();
+                return ClientSendAllSchemasServerCodec.EncodeResponse();
+            }));
+
+            // add a schema that needs to be pushed to the cluster
+            schemas.Add(GetSchema("typeName", "fieldName"), false);
+
+            // capture the schema, prepare the message, etc
+            var task0 = schemas.PublishAsync();
+
+            // capture the schema, wait for task0
+            var task1 = schemas.PublishAsync();
+
+            // add another schema
+            schemas.Add(GetSchema("otherTypeName", "otherFieldName"), false);
+
+            // unlock task0
+            semaphore.Release();
+
+            await task0; // send schema
+            await task1; // going to do nothing - schema already published
+
+            Assert.That(count, Is.EqualTo(1));
+
+            // but there is another schema
+            var task2 = schemas.PublishAsync();
+            semaphore.Release();
+            await task2;
+
+            Assert.That(count, Is.EqualTo(2));
+        }
+
+        // test the 'finally' block in Schemas.PublishAsyncReallyAsync
+        //
+        [Test]
+        public async Task ConcurrentPublish3()
+        {
+            var semaphore = new SemaphoreSlim(0, 1);
+            var count = 0;
+
+            var schemas = new Schemas(new ClusterMessagingStub(requestMessage =>
+            {
+                if (Interlocked.Increment(ref count) == 1)
+                    throw new Exception("bang!");
+                Assert.That(requestMessage.MessageType, Is.EqualTo(ClientSendSchemaCodec.RequestMessageType));
+                return Task.FromResult(ClientSendAllSchemasServerCodec.EncodeResponse());
+            }));
+
+            // add a schema that needs to be pushed to the cluster
+            schemas.Add(GetSchema("typeName", "fieldName"), false);
+
+            // publish once, will fail
+            await AssertEx.ThrowsAsync<Exception>(async () => await schemas.PublishAsync());
+            Assert.That(count, Is.EqualTo(1));
+
+            // publish again, will succeed *and* send a message
+            await schemas.PublishAsync();
+            Assert.That(count, Is.EqualTo(2));
+        }
+    }
+}
---
+++ src/Hazelcast.Net/Serialization/Compact/Schema.cs
@@ -0,0 +1,279 @@
+﻿// Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Hazelcast.Exceptions;
+
+// 'Schema' conflicts with System.Xml.Schema - well, yes.
+#pragma warning disable CA1724  
+
+namespace Hazelcast.Serialization.Compact
+{
+    /// <summary>
+    /// Represents a compact serialization schema.
+    /// </summary>
+    internal class Schema : IIdentifiedDataSerializable, IEquatable<Schema>
+    {
+        private SortedDictionary<string, SchemaField> _fieldsMap;
+        private Dictionary<string, SchemaField>? _fieldsMapInvariant;
+
+        /// <inheritdoc />
+        public int FactoryId => CompactSerializationHook.Constants.FactoryId;
+
+        /// <inheritdoc />
+        public int ClassId => CompactSerializationHook.Constants.ClassIds.Schema;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Schema"/> class.
+        /// </summary>
+        // TODO: remove pragma when Initialize has the attribute with C# 9
+#pragma warning disable CS8618
+        internal Schema()
+#pragma warning restore CS8618
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Schema"/> class.
+        /// </summary>
+        // TODO: remove pragma when Initialize has the attribute with C# 9
+#pragma warning disable CS8618
+        public Schema(string typeName, IEnumerable<SchemaField> typeFields)
+#pragma warning restore CS8618
+        {
+            if (string.IsNullOrWhiteSpace(typeName)) throw new ArgumentException(ExceptionMessages.NullOrEmpty);
+            if (typeFields == null) throw new ArgumentNullException(nameof(typeFields));
+            Initialize(typeName, typeFields);
+        }
+
+        /// <summary>
+        /// Gets the identifier of the schema.
+        /// </summary>
+        public long Id { get; private set; }
+
+        /// <summary>
+        /// Gets the compact type-name.
+        /// </summary>
+        public string TypeName { get; private set; } = null!; // null! else warning in ctor, property set in Initialize()
+
+        /// <summary>
+        /// Gets the ordered schema fields.
+        /// </summary>
+        public IReadOnlyList<SchemaField> Fields { get; private set; } = null!; // null! else warning in ctor, property set in Initialize()
+
+        /// <summary>
+        /// Tries to get a field.
+        /// </summary>
+        /// <param name="name">The name of the field.</param>
+        /// <param name="field">When this method returns, contains the <see cref="SchemaField"/> identified by the <paramref name="name"/>,
+        /// if any, or <c>null</c> if no field exists with this name.</param>
+        /// <param name="caseSensitive">Whether to perform a case-sensitive lookup.</param>
+        /// <returns><c>true</c> if a field with the specified <paramref name="name"/> was found; otherwise <c>false</c>.</returns>
+        internal bool TryGetField(string name, [NotNullWhen(true)] out SchemaField field, bool caseSensitive = true)
+        {
+            if (caseSensitive) return _fieldsMap.TryGetValue(name, out field);
+            _fieldsMapInvariant ??= new Dictionary<string, SchemaField>(_fieldsMap, StringComparer.OrdinalIgnoreCase);
+            return _fieldsMapInvariant.TryGetValue(name, out field);
+        }
+        
+        /// <summary>
+        /// Gets the size in bytes of value (non-nullable) fields.
+        /// </summary>
+        internal int ValueFieldLength { get; private set; }
+
+        /// <summary>
+        /// Gets the number of reference (nullable) fields.
+        /// </summary>
+        internal int ReferenceFieldCount { get; private set; }
+
+        /// <summary>
+        /// Whether the schema has reference (nullable) fields.
+        /// </summary>
+        internal bool HasReferenceFields => ReferenceFieldCount > 0;
+
+        //TODO: enable the attribute and get rid of the nullable warnings with C# 9
+        //[System.Diagnostics.CodeAnalysis.MemberNotNull(nameof(_fieldsMap))]
+        private void Initialize(string typeName, IEnumerable<SchemaField> typeFields)
+        {
+            TypeName = typeName;
+
+            // the sorted set of fields, which will be used for fingerprinting - needs to be ordered
+            // exactly in the same way as Java, which uses Comparator.naturalOrder() i.e. "natural
+            // order", and good luck finding a definition for this, so we're going with whatever is
+            // default in C# and hope it works.
+            var fieldsMap = new SortedDictionary<string, SchemaField>();
+
+            // ensure no duplicate field name
+            var fieldNames = new HashSet<string>();
+
+            // different type of fields
+            List<SchemaField>? booleanFields = null;
+            List<SchemaField>? valueFields = null;
+            List<SchemaField>? referenceFields = null;
+
+            // build the fields map, which is sorted (see above)
+            foreach (var field in typeFields)
+            {
+                if (!fieldNames.Add(field.FieldName))
+                    throw new ArgumentException($"Fields contain duplicate field name {field.FieldName}.", nameof(typeFields));
+                fieldsMap[field.FieldName] = field;
+            }
+
+            foreach (var field in fieldsMap.Values)
+            {
+                if (field.Kind == FieldKind.Boolean)
+                    (booleanFields ??= new List<SchemaField>()).Add(field);
+                else if (field.Kind.IsValueType())
+                    (valueFields ??= new List<SchemaField>()).Add(field);
+                else
+                    (referenceFields ??= new List<SchemaField>()).Add(field);
+            }
+
+            var offset = 0;
+
+            if (valueFields != null)
+            {
+                // value fields come first, ordered by size DESC then by name ASC
+                var fields = valueFields
+                    .Select(x => (x, x.Kind.GetValueTypeSize()))
+                    .OrderByDescending(x => x.Item2)
+                    .ThenBy(x => x.Item1.FieldName);
+                foreach (var (field, size) in fields)
+                {
+                    field.Offset = offset;
+                    offset += size;
+                }
+            }
+
+            if (booleanFields != null)
+            {
+                // boolean fields come next, ordered by name ASC
+                byte bitOffset = 0;
+                var fields = booleanFields.OrderBy(x => x.FieldName);
+                foreach (var field in fields)
+                {
+                    field.Offset = offset;
+                    field.BitOffset = bitOffset;
+                    bitOffset += 1;
+                    if (bitOffset == 8)
+                    {
+                        bitOffset = 0;
+                        offset += 1;
+                    }
+                }
+
+                if (bitOffset != 0) offset += 1;
+            }
+
+            if (referenceFields != null)
+            {
+                // reference fields come last, ordered by name ASC
+                var index = 0;
+                var fields = referenceFields.OrderBy(x => x.FieldName);
+                foreach (var field in fields)
+                {
+                    field.Index = index++;
+                }
+            }
+
+            ValueFieldLength = offset;
+            ReferenceFieldCount = referenceFields?.Count ?? 0;
+
+            Fields = fieldsMap.Values.ToArray();
+            _fieldsMap = fieldsMap;
+
+            Id = ComputeId();
+        }
+
+        private long ComputeId()
+        {
+            var fingerprint = RabinFingerprint.InitialValue;
+            fingerprint = RabinFingerprint.Fingerprint(fingerprint, TypeName);
+            fingerprint = RabinFingerprint.Fingerprint(fingerprint, Fields.Count);
+            foreach (var field in Fields) // fields are ordered for reproducible fingerprint
+            {
+                fingerprint = RabinFingerprint.Fingerprint(fingerprint, field.FieldName);
+                fingerprint = RabinFingerprint.Fingerprint(fingerprint, (int)field.Kind);
+            }
+
+            return (long) fingerprint;
+        }
+
+        /// <inheritdoc />
+        public void WriteData(IObjectDataOutput output)
+        {
+            if (output == null) throw new ArgumentNullException(nameof(output));
+
+            output.WriteString(TypeName);
+            output.WriteInt(Fields.Count);
+            foreach (var field in Fields)
+            {
+                output.WriteString(field.FieldName);
+                output.WriteInt((int)field.Kind);
+            }
+        }
+
+        /// <inheritdoc />
+        public void ReadData(IObjectDataInput input)
+        {
+            if (input == null) throw new ArgumentNullException(nameof(input));
+
+            var typeName = input.ReadString();
+
+            var fieldCount = input.ReadInt();
+            var typeFields = new SchemaField[fieldCount];
+            for (var i = 0; i < fieldCount; i++)
+            {
+                var name = input.ReadString();
+                var kind = FieldKindEnum.Parse(input.ReadInt());
+                typeFields[i] = new SchemaField(name, kind);
+            }
+
+            Initialize(typeName, typeFields);
+        }
+
+        /// <inheritdoc />
+        public bool Equals(Schema? other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id;
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object? obj)
+            => obj is Schema other && Equals(other);
+
+        /// <inheritdoc />
+        // ReSharper disable once NonReadonlyMemberInGetHashCode - it is in fact readonly
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public static bool operator ==(Schema? left, Schema? right)
+            // ReSharper disable once MergeConditionalExpression - no, cleaner that way
+            => left is null ? right is null : left.Equals(right);
+
+        public static bool operator !=(Schema? left, Schema? right)
+            => !(left == right);
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"Schema(TypeName=\"{TypeName}\", Id={Id})";
+        }
+    }
+}
