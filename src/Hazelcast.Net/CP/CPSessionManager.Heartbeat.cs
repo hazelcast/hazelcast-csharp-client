c02dc69b38cf325588a4ee68daba421a9e4a4be9
Stephan <stephane.gay@hazelcast.com>
2022-05-09 13:46:46 +0200

    PR fixes

--- src/Hazelcast.Net/CP/CPSessionManager.Heartbeat.cs
+++ src/Hazelcast.Net/CP/CPSessionManager.Heartbeat.cs
@@ -13,10 +13,8 @@
 // limitations under the License.
 
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Hazelcast.Core;
