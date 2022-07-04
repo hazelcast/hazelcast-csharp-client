e7da53c121094434bf5c7f08e2ac87131bf756a6
Stephan <stephane.gay@hazelcast.com>
2022-07-04 16:43:23 +0200

    Compact - Introduce options + some interfaces

---
+++ src/Hazelcast.Net/Core/Symbols.cs
@@ -0,0 +1,68 @@
+﻿// Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Hazelcast.Core
+{
+    /// <summary>
+    /// Provides helper methods to manipulate symbols.
+    /// </summary>
+    internal static class Symbols
+    {
+        /// <summary>
+        /// Represents just any type.
+        /// </summary>
+        public class AnyType
+        { }
+
+        /// <summary>
+        /// Gets a method <see cref="MethodInfo"/>.
+        /// </summary>
+        /// <param name="a">The method.</param>
+        /// <returns>The corresponding <see cref="MethodInfo"/>.</returns>
+        public static MethodInfo GetMethodInfo(Action a)
+            => a.Method;
+
+        /// <summary>
+        /// Gets a method generic <see cref="MethodInfo"/>.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <returns>The corresponding <see cref="MethodInfo"/>.</returns>
+        public static MethodInfo GetGenericMethodInfo<TResult>(Func<TResult> method)
+            => method.Method.GetGenericMethodDefinition();
+
+        /// <summary>
+        /// Gets a method generic <see cref="MethodInfo"/>.
+        /// </summary>
+        /// <param name="method">The method.</param>
+        /// <returns>The corresponding <see cref="MethodInfo"/>.</returns>
+        public static MethodInfo GetGenericMethodInfo<T1, TResult>(Func<T1, TResult> method)
+            => method.Method.GetGenericMethodDefinition();
+
+        public static TResult Invoke<T1, TResult>(Func<T1, TResult> f, Type[] types, object[] parameters)
+        {
+            return (TResult)f.Method.GetGenericMethodDefinition().MakeGenericMethod(types).Invoke(null, parameters);
+        }
+
+        public class StaticFuncSymbol<T1, TResult>
+        {
+            private readonly MethodInfo _methodInfo;
+            public StaticFuncSymbol(Func<T1, TResult> method) => _methodInfo = method.Method.GetGenericMethodDefinition();
+            public TResult Invoke(Type type, T1 param) => (TResult)_methodInfo.MakeGenericMethod(type).Invoke(null, new object[] { param });
+        }
+    }
+}
---
+++ src/Hazelcast.Net/Serialization/Compact/CompactRegistration.cs
@@ -0,0 +1,97 @@
+﻿// Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#nullable enable
+
+using System;
+
+namespace Hazelcast.Serialization.Compact
+{
+    /// <summary>
+    /// Represents a compact serializer registration.
+    /// </summary>
+    internal sealed class CompactRegistration
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompactRegistration"/> class.
+        /// </summary>
+        /// <param name="serializedType">The serialized type.</param>
+        /// <param name="serializer">The serializer for the serialized type.</param>
+        /// <param name="schema">The optional schema for the serialized type.</param>
+        /// <param name="typeName">The schema type name of the serialized type.</param>
+        /// <param name="isClusterSchema">Whether the schema is considered to be, at configuration time, already known by the cluster.</param>
+        private CompactRegistration(Type serializedType, CompactSerializerAdapter serializer, Schema? schema, string typeName, bool isClusterSchema)
+        {
+            // assume args are valid, *we* create the registrations
+
+            SerializedType = serializedType;
+            Serializer = serializer;
+            Schema = schema;
+            TypeName = typeName;
+            IsClusterSchema = isClusterSchema;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompactRegistration"/> class.
+        /// </summary>
+        /// <param name="serializedType">The serialized type.</param>
+        /// <param name="serializer">The serializer for the serialized type.</param>
+        /// <param name="typeName">The schema type name of the serialized type.</param>
+        /// <param name="isClusterSchema">Whether the schema is considered to be, at configuration time, already known by the cluster.</param>
+        public CompactRegistration(Type serializedType, CompactSerializerAdapter serializer, string typeName, bool isClusterSchema)
+            : this(serializedType, serializer, null, typeName, isClusterSchema)
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CompactRegistration"/> class.
+        /// </summary>
+        /// <param name="serializedType">The serialized type.</param>
+        /// <param name="serializer">The serializer for the serialized type.</param>
+        /// <param name="schema">The schema for the serialized type.</param>
+        /// <param name="isClusterSchema">Whether the schema is considered to be, at configuration time, already known by the cluster.</param>
+        public CompactRegistration(Type serializedType, CompactSerializerAdapter serializer, Schema schema, bool isClusterSchema)
+            : this(serializedType, serializer, schema, schema.TypeName, isClusterSchema)
+        { }
+
+        /// <summary>
+        /// Gets the schema type name of the serialized type.
+        /// </summary>
+        public string TypeName { get; }
+
+        /// <summary>
+        /// Gets the serialized type.
+        /// </summary>
+        public Type SerializedType { get; }
+
+        /// <summary>
+        /// Gets the schema, if any.
+        /// </summary>
+        public Schema? Schema { get; }
+
+        /// <summary>
+        /// Whether the registration provides a schema.
+        /// </summary>
+        public bool HasSchema => Schema != null;
+
+        /// <summary>
+        /// Gets the serializer for the serialized type, if any.
+        /// </summary>
+        public CompactSerializerAdapter Serializer { get; }
+
+        /// <summary>
+        /// Whether the schema is considered to be, at configuration time, already known by the cluster.
+        /// </summary>
+        public bool IsClusterSchema { get; }
+    }
+}
