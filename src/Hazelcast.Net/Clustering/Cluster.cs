bb57e4293fff525be6574d3d039e1c4468dc53ed
Stephan <stephane.gay@hazelcast.com>
2022-07-13 14:50:20 +0200

    Compact - Wire Compact Serializer

--- src/Hazelcast.Net/Clustering/Cluster.cs
+++ src/Hazelcast.Net/Clustering/Cluster.cs
@@ -81,6 +81,8 @@ namespace Hazelcast.Clustering
         {
             // beware! assigning multicast handlers *must* use +=
 
+            Messaging.SendingMessage += SerializationService.PrepareForSendingMessage;
+
             // wire members
             Connections.ConnectionOpened += (conn, isFirstEver, isFirst, isNewCluster) => { Members.AddConnection(conn, isNewCluster); return default; };
             Connections.ConnectionClosed += async conn => { await Members.RemoveConnectionAsync(conn).CfAwait(); };
