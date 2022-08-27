118f0141f8750567ca6263be6c2b8019a71519a0
Stephan <stephane.gay@hazelcast.com>
2022-08-27 17:36:13 +0200

    Fix failover unstable tests

--- src/Hazelcast.Net.Testing/Remote/Resources.Designer.cs
+++ src/Hazelcast.Net.Testing/Remote/Resources.Designer.cs
@@ -72,45 +72,9 @@ namespace Hazelcast.Testing.Remote {
         ///    &lt;property name=&quot;hazelcast.cache.invalidation.batch.size&quot;&gt;10&lt;/property&gt;
         ///    &lt;property name=&quot;haze [rest of string was truncated]&quot;;.
         /// </summary>
-        internal static string alternative {
-            get {
-                return ResourceManager.GetString("alternative", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;
-        ///&lt;hazelcast xmlns=&quot;http://www.hazelcast.com/schema/config&quot;
-        ///           xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot;
-        ///           xsi:schemaLocation=&quot;http://www.hazelcast.com/schema/config
-        ///           http://www.hazelcast.com/schema/config/hazelcast-config-4.0.xsd&quot;&gt;
-        ///
-        ///  &lt;properties&gt;
-        ///    &lt;property name=&quot;hazelcast.map.invalidation.batch.enabled&quot;&gt;false&lt;/property&gt;
-        ///    &lt;property name=&quot;hazelcast.cache.invalidation.batch.size&quot;&gt;10&lt;/property&gt;
-        ///    &lt;property name=&quot;haze [rest of string was truncated]&quot;;.
-        /// </summary>
         internal static string hazelcast {
             get {
                 return ResourceManager.GetString("hazelcast", resourceCulture);
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot;?&gt;
-        ///&lt;hazelcast xmlns=&quot;http://www.hazelcast.com/schema/config&quot;
-        ///           xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot;
-        ///           xsi:schemaLocation=&quot;http://www.hazelcast.com/schema/config
-        ///           http://www.hazelcast.com/schema/config/hazelcast-config-4.0.xsd&quot;&gt;
-        ///
-        ///  &lt;properties&gt;
-        ///    &lt;property name=&quot;hazelcast.map.invalidation.batch.enabled&quot;&gt;false&lt;/property&gt;
-        ///    &lt;property name=&quot;hazelcast.cache.invalidation.batch.size&quot;&gt;10&lt;/property&gt;
-        ///    &lt;property name=&quot;haze [rest of string was truncated]&quot;;.
-        /// </summary>
-        internal static string partition {
-            get {
-                return ResourceManager.GetString("partition", resourceCulture);
             }
         }
     }
