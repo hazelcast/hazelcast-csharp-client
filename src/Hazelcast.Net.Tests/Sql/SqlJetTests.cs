52b7a2e210365644a0e85c1391534aa2062908a2
Stephan <stephane.gay@hazelcast.com>
2023-01-02 13:44:46 +0100

    Fix: back to separate cluster member jet-enabled config

--- src/Hazelcast.Net.Tests/Sql/SqlJetTests.cs
+++ src/Hazelcast.Net.Tests/Sql/SqlJetTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+﻿// Copyright (c) 2008-2021, Hazelcast, Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -25,6 +25,8 @@ namespace Hazelcast.Tests.Sql
     [ServerCondition("[5.0,)")] // only on server 5.0 and above
     public class SqlJetTests : SqlTestBase
     {
+        protected override bool EnableJet => true;
+
         [Test]
         public async Task GenerateSeries()
         {
