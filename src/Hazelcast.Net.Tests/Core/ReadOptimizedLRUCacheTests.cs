8e8ea22062eda50b278cc073320a6e78182743a1
emreyigit <emre.yigit@hazelcast.com>
2023-02-21 17:12:50 +0300

    LRU cache first part.

---
+++ src/Hazelcast.Net.Tests/Core/ReadOptimizedLRUCacheTests.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Hazelcast.Core;
+
+namespace Hazelcast.Tests.Core;
+
+public class ReadOptimizedLruCacheTests
+{
+    [Test]
+    public void TestWrongCapacityThrows()
+    {
+        Assert.Throws<ArgumentException>(() => new ReadOptimizedLruCache<int, int>(-1, 0));
+        Assert.Throws<ArgumentException>(() => new ReadOptimizedLruCache<int, int>(1, 0));
+        Assert.Throws<ArgumentException>(() => new ReadOptimizedLruCache<int, int>(0, -1));
+        Assert.Throws<ArgumentException>(() => new ReadOptimizedLruCache<int, int>(10, 9));
+        var correct = new ReadOptimizedLruCache<int, int>(10, 15);
+    }
+
+    [Test]
+    public void TestAdd()
+    {
+        var cache = new ReadOptimizedLruCache<string, string>(10, 15);
+
+        cache.Add("1", "1");
+        cache.Add("2", null);
+
+        Assert.Throws<ArgumentNullException>(() => cache.Add(null, "1"));
+    }
+
+    [Test]
+    public async Task TestAddAndGetRefreshEntry()
+    {
+        var cache = new ReadOptimizedLruCache<int, int>(10, 15);
+
+        cache.Add(1, 1);
+        cache.dictionary.TryGetValue(1, out var entry);
+        var firstTouch = entry.LastTouch;
+        await Task.Delay(100);
+        var keyExist = cache.TryGetValue(1, out var _);
+
+        Assert.True(keyExist);
+
+        var secondTouch = entry.LastTouch;
+
+        Assert.Greater(secondTouch, firstTouch);
+    }
+
+    [Test]
+    public void TestEviction()
+    {
+        var capacity = 10;
+        var threshold = 15;
+        var cache = new ReadOptimizedLruCache<int, int>(capacity, threshold);
+
+        for (var i = 0; i < threshold; i++)
+            cache.Add(i, i);
+
+        Assert.AreEqual(cache.dictionary.Count, threshold);
+        // Last stroke to break camel's back
+        cache.Add(15, 15);
+        // The cache size exceeds the threshold, so remove the least recent used # of (threshold - capacity) items
+        Assert.AreEqual(capacity, cache.dictionary.Count);
+    }
+
+    [Test]
+    public void TestNoOperationAfterDispose()
+    {
+        var cache = new ReadOptimizedLruCache<int, int>(5, 10);
+        cache.Dispose();
+
+        Assert.Throws<ObjectDisposedException>(() => cache.Add(1, 1));
+        Assert.Throws<ObjectDisposedException>(() => cache.TryGetValue(1, out _));
+    }
+}
---
+++ src/Hazelcast.Net/Core/ReadOptimizedLruCache.cs
@@ -0,0 +1,119 @@
+﻿// Copyright (c) 2008-2022, Hazelcast, Inc. All Rights Reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
+using Hazelcast.Models;
+
+namespace Hazelcast.Core;
+
+internal class ReadOptimizedLruCache<TKey, TValue> : IDisposable
+{
+    // internal only for tests
+    internal readonly ConcurrentDictionary<TKey, TimeBasedEntry<TValue>> dictionary = new();
+    private readonly SemaphoreSlim _cleaningSlim = new (1, 1);
+    private readonly int _capacity;
+    private readonly int _threshold;
+    private int _disposed;
+
+    public ReadOptimizedLruCache(int capacity, int threshold)
+    {
+        if (capacity <= 0 || threshold <= 0) throw new ArgumentException("Threshold value or capacity cannot be negative or zero.");
+        if (capacity >= threshold) throw new ArgumentException("Threshold value cannot be less than or equal capacity.");
+
+        _capacity = capacity;
+        _threshold = threshold;
+    }
+
+
+    /// <summary>
+    /// Try get value corresponds to given key.
+    /// </summary>
+    /// <param name="key">Key</param>
+    /// <param name="val">Value</param>
+    /// <returns>True if key exists or false.</returns>
+    /// <exception cref="ArgumentNullException">If key is null.</exception>
+    public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue val)
+    {
+        if (_disposed == 1) throw new ObjectDisposedException("Cache is disposed.");
+
+        if (key is null) throw new ArgumentNullException(nameof(key));
+
+        if (dictionary.TryGetValue(key, out var entry))
+        {
+            entry.Touch();
+            val = entry.Value;
+            return true;
+        }
+
+        val = default;
+        return false;
+    }
+
+    /// <summary>
+    /// Adds the given key value to cache.
+    /// </summary>
+    /// <param name="key">Key</param>
+    /// <param name="val">Value</param>
+    /// <exception cref="ArgumentNullException">If key is null.</exception>
+    public void Add(TKey key, TValue val)
+    {
+        if (_disposed == 1) throw new ObjectDisposedException("Cache is disposed.");
+
+        if (key is null) throw new ArgumentNullException(nameof(key));
+
+        var entry = new TimeBasedEntry<TValue>(val);
+        var addVal = dictionary.GetOrAdd(key, entry);
+
+        if (!addVal.Equals(val) && dictionary.Count > _threshold)
+            DoEviction();
+    }
+
+    private void DoEviction()
+    {
+        _cleaningSlim.Wait();
+
+        try
+        {
+            if (dictionary.Count < _threshold) return;
+
+            var timestamps = dictionary
+                .OrderBy(p => p.Value.LastTouch)
+                .ToArray();
+
+            var countOfEntriesToRemoved = _threshold - _capacity;
+
+            for (var i = 0; i <= countOfEntriesToRemoved; i++)
+            {
+                dictionary.TryRemove(timestamps[i].Key, timestamps[i].Value);
+            }
+        }
+        finally
+        {
+            _cleaningSlim.Release();
+        }
+    }
+
+    public void Dispose()
+    {
+        Interlocked.CompareExchange(ref _disposed, 1, 0);
+
+        if (_disposed == 1) return;
+        dictionary.Clear();
+        _cleaningSlim.Dispose();
+    }
+}
