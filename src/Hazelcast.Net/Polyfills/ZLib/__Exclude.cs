6ff4ba736ef10d1573b22438ab379abb1c1b13ed
Stephan <sgay@pilotine.com>
2023-09-27 18:39:51 +0200

    Remove DotNetZip dependency [API-2140] (#851)

---
+++ src/Hazelcast.Net/Polyfills/ZLib/__Exclude.cs
@@ -0,0 +1,7 @@
+﻿
+#if NET6_0_OR_GREATER
+
+// ensure that Hazelcast.Polyfills.ZLib is *not* compiled for .NET 6+
+#error Hazelcast.Polyfills.ZLib code should not build for this target framework!
+
+#endif
