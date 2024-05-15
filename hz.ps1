9af3414bb74ec3c49692affd82764dc5f9cfb1cf
Emre Yigit <emre.yigit@hazelcast.com>
2024-05-15 13:32:27 +0300

    Update server version (#892)

--- hz.ps1
+++ hz.ps1
@@ -15,7 +15,7 @@
 ## Hazelcast.NET Build Script
 
 # constant
-$defaultServerVersion="5.4.0-SNAPSHOT"
+$defaultServerVersion="5.4.0"
 
 # PowerShell errors can *also* be a pain
 # see https://stackoverflow.com/questions/10666035
