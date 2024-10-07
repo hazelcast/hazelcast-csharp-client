dd595e4c379fd84f7e1d9da1e69aefe804a12e66
emreyigit <emre.yigit@hazelcast.com>
2024-10-07 11:45:38 +0300

    Review changes.

--- src/Hazelcast.Net/Models/ExpiryPolicyType.cs
+++ src/Hazelcast.Net/Models/ExpiryPolicyType.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright (c) 2008-2023, Hazelcast, Inc. All Rights Reserved.
+// Copyright (c) 2008-2024, Hazelcast, Inc. All Rights Reserved.
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
