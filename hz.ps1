31c0dcd37f46f642972a4ae263c2d1146155b12c
emreyigit <emre.yigit@hazelcast.com>
2025-09-09 11:56:19 +0300

    Fix missing .Net6 on windows-latest

--- hz.ps1
+++ hz.ps1
@@ -15,7 +15,7 @@
 ## Hazelcast.NET Build Script
 
 # constant
-$defaultServerVersion="5.5.6"
+$defaultServerVersion="5.6.0-SNAPSHOT"
 
 # PowerShell errors can *also* be a pain
 # see https://stackoverflow.com/questions/10666035
