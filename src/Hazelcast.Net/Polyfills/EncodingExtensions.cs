02a609cab178b835c9595fd208d25e55ce3341eb
emreyigit <emre.yigit@hazelcast.com>
2026-02-03 18:51:44 +0300

    Add poly fills for legacy framework

--- src/Hazelcast.Net/Polyfills/EncodingExtensions.cs
+++ src/Hazelcast.Net/Polyfills/EncodingExtensions.cs
@@ -11,10 +11,46 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+using System;
+using System.Text;
+
 namespace Hazelcast.Polyfills
 {
-    public class EncodingExtensions
+    internal static class EncodingExtensions
     {
-        
+#if NETSTANDARD2_0
+        public static unsafe int GetBytes(this Encoding encoding, ReadOnlySpan<char> chars, Span<byte> bytes)
+        {
+            fixed (char* c = chars)
+            fixed (byte* b = bytes)
+            {
+                return encoding.GetBytes(c, chars.Length, b, bytes.Length);
+            }
+        }
+
+        public static unsafe string GetString(this Encoding encoding, ReadOnlySpan<byte> bytes)
+        {
+            fixed (byte* b = bytes)
+            {
+                return encoding.GetString(b, bytes.Length);
+            }
+        }
+
+        public static unsafe void Convert(
+            this Encoder encoder,
+            ReadOnlySpan<char> chars,
+            Span<byte> bytes,
+            bool flush,
+            out int charsUsed,
+            out int bytesUsed,
+            out bool completed)
+        {
+            fixed (char* c = chars)
+            fixed (byte* b = bytes)
+            {
+                encoder.Convert(c, chars.Length, b, bytes.Length, flush, out charsUsed, out bytesUsed, out completed);
+            }
+        }
+#endif
     }
 }
